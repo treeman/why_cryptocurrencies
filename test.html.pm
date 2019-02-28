94c78d0422cb53a4430bbaf721b28778230006d0
Jonas Hietala <mail@jonashietala.se>
2019-02-28 18:59:08 +0100

    Test

---
+++ test.html.pm
@@ -0,0 +1,20 @@
+#lang pollen
+
+◊(define-meta template "homepage.html")
+◊(define-meta title "test")
+
+◊article{
+  ◊div{div}
+
+In the beginning ◊mn{wp} there was nothing.
+
+◊mndef["wp"]{
+  Some kind of...
+
+  Monster!
+}
+}
+
+Oh yes.
+
+
