d610cd9e5e79afba1fef39865a619bd0d760b47e
Jonas Hietala <mail@jonashietala.se>
2019-04-01 14:01:48 +0200

    Ignore files during deploy

--- pollen.rkt
+++ pollen.rkt
@@ -12,14 +12,30 @@
 
 (module setup racket/base
   (require file/glob)
-  (require racket/runtime-path)
   (require pollen/setup)
+  (require racket/runtime-path racket/path racket/set)
 
   (provide (all-defined-out))
 
   (define template-prefix "chapter")
   ;; splice-me needs to NOT be a block tag to allow us to inject
   (define block-tags (cons 'img default-block-tags))
+
+  ;; Ignore stuff during 'raco pollen publish'
+  (define publish-directory "/tmp/why_cryptocurrencies/")
+  (define omitted-paths
+    (list->set (map (λ (sub)
+                       (build-path publish-directory sub))
+                    `("img-src"
+                      "sass"
+                      "wip"
+                      "rkt"
+                      "plots"
+                      "clean"
+                      "sass-update"
+                      "README.md"))))
+  (define omitted-path? (λ (path)
+                           (set-member? omitted-paths path)))
 
   (define rkt-files (glob "rkt/*.rkt"))
   (define cache-watchlist rkt-files))
