92f86238bd9d05088eff3c57f7c791c479fc61b5
Jonas Hietala <mail@jonashietala.se>
2019-04-03 09:41:44 +0200

    Error page

---
+++ error.html.pm
@@ -0,0 +1,9 @@
+#lang pollen
+
+◊(define-meta title "404")
+◊(define-meta no-side-space #t)
+◊(define-meta no-section-chapters-header #t)
+◊(define-meta extra-article-class "error")
+
+I'm sorry, the page you were looking for couldn't be found.
+
