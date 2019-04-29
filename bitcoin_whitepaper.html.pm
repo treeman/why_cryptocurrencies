839e1d54479a5a42caf0eb469f289e5700096886
Jonas Hietala <mail@jonashietala.se>
2019-04-29 14:09:21 +0200

    Spell checks

--- bitcoin_whitepaper.html.pm
+++ bitcoin_whitepaper.html.pm
@@ -1,6 +1,6 @@
 #lang pollen
 
-◊(define-meta title "The Bitcoin Whitepaper")
+◊(define-meta title "The Bitcoin White paper")
 ◊(define-meta subtitle "What started it all")
 ◊(define-meta updated "2019-04-03T19:59:58+0200")
 ◊(define-meta uuid "581c4917-d862-49cb-9d01-495a9106081b")
@@ -10,20 +10,20 @@
     ◊link[bitcoin-pdf]{Bitcoin: A Peer-to-Peer Electronic Cash System}
 }
 
-The whitepaper is surprisingly easy to read and I highly recommend you read it. If you prefer a simplified explanation with annotations I can recommend ◊link[ann-wp]{this guide}. If you would rather have it in podcast form ◊link[pod-wp]{here's one}.
+The white paper is surprisingly easy to read and I highly recommend you read it. If you prefer a simplified explanation with annotations I can recommend ◊link[ann-wp]{this guide}. If you would rather have it in podcast form ◊link[pod-wp]{here's one}.
 
-Note that the whitepaper was created in 2008 and some terminology and implementation details have changed. For example ◊em{nodes} in the paper refers to mining nodes, while today most people run nodes but don't mine. But the high level description is just as true today more than 10 years later.
+Note that the white paper was created in 2008 and some terminology and implementation details have changed. For example ◊em{nodes} in the paper refers to mining nodes, while today most people run nodes but don't mine. But the high level description is just as true today more than 10 years later.
 
 
 ◊subhead{Alterations}
 
 I'm sad I have to include this but there have been suggestions to ◊link[alter-wp]{alter the whitepaper hosted on bitcoin.org}. Rewriting an academic paper and rewriting Bitcoin's history for personal business interests is of course completely unacceptable, but here we are.
 
-Therefore you might want to make sure you're reading the original unaltered whitepaper. There are those who ◊link[track-alterations]{try to keep track} of the different versions found online but it's always best to do it yourself. You can compare the pdf's sha256 hash with this:
+Therefore you might want to make sure you're reading the original unaltered white paper. There are those who ◊link[track-alterations]{try to keep track} of the different versions found online but it's always best to do it yourself. You can compare the pdf's sha256 hash with this:
 
 ◊code{b1674191a88ec5cdd733e4240a81803105dc412d6c6708d53ab94fc248f4f553}
 
-If you want to go even further the whitepaper is also embedded in the blockchain which guarantees nobody can change it. Instructions available ◊link[wp-blockchain]{on stackexchange}.
+If you want to go even further the white paper is also embedded in the blockchain which guarantees nobody can change it. Instructions available ◊link[wp-blockchain]{on stackexchange}.
 
 
 ◊(define bitcoin-pdf
