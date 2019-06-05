59f63f172b0b6ba101afb60c25a4a47cc22e28b7
Jonas Hietala <mail@jonashietala.se>
2019-06-05 09:03:25 +0200

    Undesirable writings

--- undesirable_businesses.html.pm
+++ undesirable_businesses.html.pm
@@ -8,9 +8,9 @@
 If you want to accept digital payments today you need to partner with a payment processor, for example Stripe or PayPal. Usually that's not a problem---but what if they say no?◊mn{banks}
 
 ◊ndef["banks"]{
-    While I will focus on payment processors in this chapter, the problem is much bigger than that. Often banks don't want anything to do with the business either. Sometimes they even close personal accounts of business owners---because of the nature of the business.
+    While we'll focus on payment processors in this chapter, the problem is much bigger than that. Often banks don't want anything to do with the business either. Sometimes they even close personal accounts of business owners---because of the nature of the business.
 
-    For example ◊link[chase-porn]{Chase closed hundreds of porn stars' accounts} and Wells Fargo even ◊link[wells-fargo-supporter]{closed the bank account} of a supporter of medical marijuana.
+    For example ◊link[chase-porn]{Chase closed hundreds of porn stars' accounts} and Wells Fargo ◊link[wells-fargo-supporter]{closed the bank account} of a supporter of medical marijuana.
 }
 
 This is the reality for certain businesses with high ◊link[chargeback-fraud]{chargebacks} rate or exists in a legal and ◊link[porn-bank-moral]{moral} gray area. If you view it from the payment processor's point-of-view it might make sense---they're just minimizing their risk. But that's of little comfort to the ones being rejected.
@@ -45,22 +45,55 @@ Due to this legal uncertainty most banks and payment processors simply won't tou
     Not having access to banks means they also have to store the cash themselves and they have trouble getting loans.
 }
 
+We're talking large amounts of money here, for example ◊link[pot-banks-no]{two businesses in Denver} generated $250,000 to $350,000 in monthly sales---all in cash. This all needs to be counted, recounted, transported and stored. Large amounts of cash needs large amounts of secure storage; which means renting warehouses and hiring armored trucks and armored guards.
+
+They are absolutely dependent on nearby ATMs, because the move towards cashless payments means people don't usually carry that much cash. In fact many stores have an ATM inside the store itself, to sort-of allow people to pay with credit cards.◊sn{atms}
+
+◊ndef["atms"]{
+    The ATMs are run by separate businesses and usually don't have any problems with banks.
+}
+
+Not having access to digital payments hurts, but it's not all doom and gloom. Using cash works---in physical stores. There might also be (uncertain) ◊link[state-payment-cannabis]{ways to accept digital payments} making online pot stores possible.
+
+◊(define pot-banks-no "https://www.nytimes.com/2018/01/04/magazine/where-pot-entrepreneurs-go-when-the-banks-just-say-no.html")
 ◊(define scat "https://en.wikipedia.org/wiki/Schr%C3%B6dinger%27s_cat")
 ◊(define cannabis-legality "https://en.wikipedia.org/wiki/Legality_of_cannabis_by_U.S._jurisdiction")
+◊(define state-payment-cannabis "http://stayregular.net/blog/the-state-of-payment-processing-for-cannabis-e-commerce")
 
 ◊;http://stayregular.net/blog/the-state-of-payment-processing-for-cannabis-e-commerce
 ◊;https://www.cardfellow.com/blog/credit-card-processing-for-marijuana-businesses/
 ◊;https://www.governing.com/topics/finance/sl-marijuana-businesses.html
-◊;https://www.nytimes.com/2018/01/04/magazine/where-pot-entrepreneurs-go-when-the-banks-just-say-no.html?searchResultPosition=7
 
 
 ◊subhead{Expensive and dubious workarounds}
 
-But wait a minute, some "undesirable" companies do accept credit cards? Has the problem been solved?
+But wait a minute, some "undesirable" companies do accept credit cards? Has the problem been solved? Not really.◊sn{programming-adult}
 
-Not really.
+◊ndef["programming-adult"]{
+    There was a ◊link[programming-adult-blog]{popular blog post} detailing the struggles of porn sites accepting digital payments. The post itself has been deleted because of people ◊link[programming-adult-hn]{accusing him} of admitting to fraud (the workarounds are of dubious legality). If you're savvy you might be able to find the original post on the ◊link[wayback-machine]{Wayback machine}.
+}
 
-◊;https://web.archive.org/web/20180814120031/https://dev.to/jwoertink/programming-in-the-adult-entertainment-industry-is-broken-hgn
+Porn is a business with very high rates of ◊link[chargeback-fraud]{charge backs}. Just think about it: what happens if your significant other would come across a charge to "XXX-Teens" or similar? Of course you'd exclaim "my card must've been compromised!" and you quickly call your bank and issue a charge back.
+
+Therefore, to be able to accept credit cards at all, porn sites can use several different payment processors. If the card fails at the first one, move on to the next one to try again. Each will do more shady stuff than the previous. They can for example do things like:
+
+◊ol{
+    ◊li{Request a wire transfer number.}
+    ◊li{Say they're a "social network company" instead of a "porn company".}
+    ◊li{Have a third party request the payment on their behalf.}
+}
+
+These things are on the gray area legal wise◊sn{legal?}, and so the payment processors that does this have higher fees---MUCH higher fees. While regular credit card payments may come with 1-4% fees these fees might be as high as ◊strong{30%}. That's ludicrously expensive for low margin businesses.
+
+◊ndef["legal?"]{
+    I'm not taking a stand if this practice is legal or not. Just noting that they don't really have many options.
+}
+
+
+◊(define wayback-machine "https://archive.org/web/web.php")
+◊(define programming-adult-blog "https://dev.to/jwoertink/programming-in-the-adult-entertainment-industry-is-broken-hgn")
+◊(define programming-adult-blog-wayback "https://web.archive.org/web/20180814120031/https://dev.to/jwoertink/programming-in-the-adult-entertainment-industry-is-broken-hgn")
+◊(define programming-adult-hn "https://news.ycombinator.com/item?id=17756219")
 
 
 ◊subhead{Why does it matter?}
@@ -84,7 +117,7 @@ Not really.
 ◊(define (tab . txt)
    `(span ((class "qt-tab")) ,@txt))
 
-You might wonder what does it matter if these businesses disappear. Maybe you don't gamble, don't use cannabis and find porn deplorable---which is fine. But what about other people?◊sn{privacy}
+You might wonder what does it matter if these businesses disappear. Maybe you don't gamble, don't use cannabis and find porn deplorable---which is fine. But what about the thoughts of other people?◊sn{privacy}
 
 ◊ndef["privacy"]{
     Incidentally it's also a powerful argument for privacy. Maybe you personally don't have anything to hide---but others do.
@@ -99,21 +132,22 @@ You might wonder what does it matter if these businesses disappear. Maybe you do
 }
 
 
-The cannabis business is experiencing ◊link[cannabis-growth]{explosive growth}, hinting at how many people do care. Porn is another thing that is very popular, but very few would admit they watch it. I can see why people are against these, but what's the problem with auctions (other than being an easy target for ◊link[rel-charge-back-fraud]{charge back fraud})?
+The cannabis business is experiencing ◊link[cannabis-growth]{explosive growth}, hinting at how many people do care. Porn is another thing that is very popular---but very few would admit they watch it. I can see why some people object against them... But what's the problem with auctions? (Other than being an easy target for ◊link[rel-charge-back-fraud]{charge back fraud}.)
 
-Why should people running and using ◊strong{legal} businesses be punished for arbitrary reasons? It's quite important for a society with laws that they're actually followed. Otherwise the whole system deteriorates and might eventually collapse.
+Why should people running and using ◊strong{legal} businesses be punished for arbitrary reasons? A society that punishes people for doing something legal seems insane to me. (Yes legality is different from morality, a topic we'll revisited in the chapter about ◊link[black-markets]{black markets}.)
 
 ◊(define re-education "https://www.amnesty.org/en/latest/news/2018/09/china-up-to-one-million-detained/")
 ◊(define cannabis-growth "https://www.marijuanaseo.com/cannabis-industry-statistics/")
+◊(define black-markets "/black_markets.html")
 
 
 ◊subhead{Are cryptocurrencies the solution?}
 
 Cryptocurrencies give you permissionless digital payments---it solves the problem of accepting payments very well. But it's not a full solution, at least today.
 
-As we discussed in ◊link[are-cryptos-money]{Are cryptocurrencies money?} they're still very volatile and not widely accepted. Businesses still need to convert cryptocurrencies to fiat to be able to pay their bills and salaries, and the workers in turn need to pay their bills. To sell them for fiat you still need to go through exchanges who, you guessed it, can ◊link[coinbase-close-accounts]{refuse to do business with you}.
+As we discussed in ◊link[are-cryptos-money]{Are cryptocurrencies money?} they're still very volatile and not widely accepted. Businesses still need to convert cryptocurrencies to fiat to be able to pay their bills and salaries, and the workers in turn need to pay their bills. To sell them for fiat you still need to go through exchanges who---you guessed it---can ◊link[coinbase-close-accounts]{refuse to do business with you}.
 
-While cryptocurrencies improves the situation today◊sn{localbitcoins}, we would really need to bypass all third-parties for a great solution. This means you should be able to pay all expenses with cryptocurrencies---so you can't get blocked by an exchange.
+While cryptocurrencies improves the situation today◊sn{localbitcoins}, we would really need to bypass all third-parties for a great solution. This means you should be able to pay all expenses with cryptocurrencies---so you can't get blocked by a third party anywhere on the line.
 
 But there are related problems cryptocurrencies can't solve. Banks serve a very important function: they lend businesses money, and there's no good solution if they say no. It can also be more secure to let banks store large sums of money than keeping it yourself.
 
