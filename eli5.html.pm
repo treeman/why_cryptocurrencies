0f2403ce0054bd8d8411b232c508abab7ad09ddb
Jonas Hietala <mail@jonashietala.se>
2019-06-17 08:00:50 +0200

    More links

--- eli5.html.pm
+++ eli5.html.pm
@@ -10,28 +10,28 @@ While cryptocurrencies are mostly seen as speculative assets---get rich quick sc
 ◊ul{
     ◊li{◊strong{Cheaper payments}
 
-        Merchants have to pay a 1--4% fee for every credit card transaction, cryptocurrency transactions only come with a small fixed fee.◊sn{bitcoin-fees}}
+        Merchants have to pay a 1--4% fee for every credit card transaction, cryptocurrency transactions only come with a ◊link[cheaper-fees]{small fixed fee.}◊sn{bitcoin-fees}}
 
     ◊li{◊strong{Settles faster}
 
-        You receive money under an hour, while it takes days to receive other digital payments which can also be reversed weeks or months later.
+        You receive money ◊link[settlement-time]{under an hour}, while it takes days to receive other digital payments---which can also be reversed weeks or months later.
 
-        This also means merchants don't have to worry about having a purchase reversed, which usually means they have to swallow the loss.
+        This means merchants don't have to worry about having a purchase reversed, which usually means they have to swallow the loss.◊sn{chargeback-fraud}
         }
 
     ◊li{◊strong{For anyone}
 
-        Cryptocurrencies makes digital payments available for anyone. For example businesses who cannot accept credit cards or people without a bank account.
+        Cryptocurrencies makes digital payments available for anyone. For example businesses ◊link[undesirable]{who cannot accept credit cards} or people ◊link[unbanked]{without a bank account}.
         }
 
     ◊li{◊strong{No risk of having your account frozen}
 
-        There's a big risk for merchants who can get their account---and their funds---frozen at any time, possibly destroying their business.
+        There's a big risk for merchants who can get their account---and their funds---◊link[freeze]{frozen at any time}, possibly destroying their business.
     }
 
     ◊li{◊strong{Send to anywhere in the world}
 
-        As long as there's internet you can send money anywhere, basically instantly and for free.
+        As long as there's internet you can ◊link[borderless]{send money anywhere}, basically instantly and for free.
     }
 
     ◊li{◊strong{Reduces currency manipulation}
@@ -47,20 +47,33 @@ While cryptocurrencies are mostly seen as speculative assets---get rich quick sc
 
 Of course cryptocurrencies don't solve everything perfectly, they're held back by adoption and price volatility in particular. And as with all new technology they will be associated with positive and negative change.
 
-If you want to learn more just continue reading, or go to ◊link[what-chapter]{What is a cryptocurrency?} and continue from there. You can also ◊link[home]{jump} to whatever chapter interests you---they're supposed to be self-contained.
+If you want to learn more and see more examples just continue reading, or go to ◊link[what-chapter]{What is a cryptocurrency?} and continue from there. You can also ◊link[home]{jump} to whatever chapter interests you---they're supposed to be self-contained.
 
 ◊ndef["bitcoin-fees"]{
     If you've heard about the ridiculously high Bitcoin fees then don't worry---it's the exception not the rule. Please read the chapter ◊link[cheaper-chapter]{Cheaper & faster} for more info.
 }
 
 ◊ndef["bitcoin-privacy"]{
-    Please note that Bitcoin (and most other cryptocurrencies) are only pseudo-anonymous. There are others---like Monero---who improves the situation. ◊link[anonymous]{Read this} for more info.
+    Please note that Bitcoin (and most other cryptocurrencies) are only ◊em{pseudo-anonymous}. There are others---like Monero---who improves the situation. ◊link[anonymous]{Read this} for more info.
+}
+
+◊ndef["chargeback-fraud"]{
+    This is known as ◊em{◊link[chargeback]{charge back fraud}} or ◊em{friendly fraud}.
 }
 
 ◊(define home "/")
 ◊(define book-chapter "/about_the_book.html")
 ◊(define what-chapter "/what_is_a_cryptocurrency.html")
-◊(define properties-chapter "/properties_of_a_cryptocurrency.html")
-◊(define cheaper-chapter "/cheaper_faster.html")
-◊(define anonymous "/properties_of_a_cryptocurrency.html#anonymous")
 
+◊(define cheaper-chapter "/cheaper_faster.html")
+◊(define chargeback "/cheaper_faster.html#charge-back-fraud")
+◊(define cheaper-fees "/cheaper_faster.html#fee-comparisons")
+◊(define settlement-time "/cheaper_faster.html#speed-comparisons")
+
+◊(define freeze "/freezing_of_merchant_accounts.html")
+
+◊(define properties-chapter "/properties_of_a_cryptocurrency.html")
+◊(define anonymous "/properties_of_a_cryptocurrency.html#anonymous")
+◊(define undesirable "/undesirable_businesses.html")
+◊(define unbanked "/for_the_unbanked.html")
+◊(define borderless "/a_borderless_currency.html")
