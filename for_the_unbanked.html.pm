a65b716bce38f5c9ed33a8a740f5173836a144a5
Jonas Hietala <mail@jonashietala.se>
2019-08-25 16:19:17 +0200

    Unbanked

--- for_the_unbanked.html.pm
+++ for_the_unbanked.html.pm
@@ -61,6 +61,9 @@ Almost half of all unbanked live in just seven countries: China, India, Indonesi
 While the fraction of unbanked adults are high in the developing countries, with poor access to electricity and internet, a surprisingly large number of unbanked live in developed countries. For example USA has 18 million unbanked and in France there are 3 million unbanked.
 
 
+◊subhead{Why are they unbanked?}
+
+
 ◊subhead{Unbanked with internet}
 
 The question "But how do the unbanked pay for internet?" is always asked when discussing the unbanked. The report has this to say:
@@ -92,7 +95,7 @@ So about ◊strong{420 million} do have access to internet while being unbanked.
 
 ◊subhead{What's the problem?}
 
-There are two big problems with unbanked as I see it. One on a country level and one for individuals:
+There are two big problems with unbanked as I see it. One on a country level and one for unbanked individuals:
 
 ◊ol{
     ◊li{Countries miss out on economic growth}
@@ -125,33 +128,23 @@ Consider a country with a high ratio of unbanked versus a country where most hav
 
 ◊h3{Shut-out from society}
 
+Now it might be relatively fine for you to live in a country without using a bank account---if most don't have one either. But what if you live in a country where digital payments are an integral part of society?
 
-◊ol{
-    ◊li{Missing out on economic growth}
-    ◊li{Shut-out from society}
-    ◊li{Digital payments is a privacy nightmare}
+For example here in Sweden almost everyone and everything uses digital payments. It would be ◊strong{extremely} difficult for you to live here without a credit card or a bank to pay your bills.
+
+As I went on a small business trip just a couple of weeks ago to the middle parts of Sweden I accidentally forgot my wallet---with my credit cards and identification card. I borrowed some cash from a friend, thinking I could use them to pay for lunch and dinner, but surprisingly most restaurants didn't accept cash. So I still had to ask my co-workers to pay for me...◊sn{moved-to-sweden}
+
+◊ndef["moved-to-sweden"]{
+    Similar story of a co-worker who moved to Sweden. There was a delay for him to get his social security number, and thus his bank account, so he had to be taken care of by people at work until it got sorted out.
 }
 
-◊todo{Rewrite all this}
-
-
-One of the best ways to fight poverty is economic growth. Ability to make digital payments is a cornerstone for inclusion into the ever-more digitalized world.
-
-◊qt{
-    The benefits from financial inclusion can be wide ranging. For example, studies have shown that mobile money services--- which allow users to store and transfer funds through a mobile phone---can help improve people’s income earning potential and thus reduce poverty.
-
-    A study in Kenya found that access to mobile money services delivered big benefits, especially for women. It enabled women-headed households to increase their savings by more than a fifth; allowed 185,000 women to leave farming and develop business or retail activities; and helped reduce extreme poverty among women-headed households by 22 percent.
-}
-
-But the problem isn't exclusive to poor countries. In Sweden for example almost everyone and everything uses digital payments. Some shops don't accept cash anymore (and they talk about it like it's a benefit to us customers) and they prefer to use Swish instead---which unfortunately also requires a bank account.
-
-What happens to the people without credit cards in Sweden? Or perhaps tourists if their card doesn't work? They're luckily few... But they're people too! With the current development they might not even be able to use public bathrooms!◊sn{public-bathroom}
+I don't really know how people without a bank account manage in Sweden. Or what tourists will do if their credit card stops working? With the current development they might not even be able to use public bathrooms!◊sn{public-bathroom}
 
 ◊ndef["public-bathroom"]{
-    In fact when I last visited a public bathroom in a shopping mall they had a new payment system there. Instead of inserting a coin to get the door open---you guessed it---you had to pay with a credit card or with Swish. I guess the kids, who aren't allowed to have a credit card or Swish yet, can go pee in the corner.
+    In fact when I last visited a public bathroom in a shopping mall they had a new payment system there. Instead of inserting a coin to get the door open---you guessed it---you had to pay with a credit card or with Swish. I guess the kids or tourists, who don't have a credit card or Swish yet, has to hold it in.
 }
 
-In China mobile payments are growing like mad. If you're a tourist you need to get them, otherwise you'll have a tough time. This in combination with China's ◊em{social credit}, which ranks people's behaviors to make sure they're in line with the party, is a recipe for disaster. If you score badly you might not be allowed to fly and maybe you'll lose the privilege of digital payments.
+In China mobile payments are growing like mad. If you're a tourist you should get them, otherwise you'll have a tough time. This in combination with China's ◊em{social credit}, which ranks people's behaviors to make sure they're in line with the party, is a recipe for disaster. If you score badly you might not be allowed to fly and maybe you'll lose the privilege of digital payments.
 
 ◊; WeChat is Watching: Living in China with the app that knows everything about me
 ◊; https://news.ycombinator.com/item?id=20173549
@@ -168,6 +161,9 @@ In China mobile payments are growing like mad. If you're a tourist you need to g
 
 ◊subhead{Cryptocurrencies are for everyone}
 
-Cryptocurrencies are ◊em{permissionless}, you don't need permission from anyone to use it, meaning everyone can use it.
+Cryptocurrencies are ◊em{permissionless}, you don't need permission from anyone to use it, meaning everyone are allowed to use it. It doesn't matter if you're homeless, a porn star or a tourist---you can always have access to digital payments.
 
+There are also no extra fees except for the small cost to
+
+Of course "just use cryptocurrencies" isn't the answer to all problems for the unbanked. They aren't a replacement for banks in the first place. But it is a helpful tool which, as adoption grow, might be helpful for many.
 
