4766135142d3c2c67bc53bbab312f31ea8921c73
Jonas Hietala <mail@jonashietala.se>
2019-09-02 07:59:51 +0200

    Rough outline

--- protection_against_government_seizures.html.pm
+++ protection_against_government_seizures.html.pm
@@ -6,3 +6,72 @@
 ◊(define-meta uuid "01795fb4-c99a-4168-8685-73ad86ed7c0c")
 
 TODO
+
+◊subhead{India voids 500 and 1000 rupee bills}
+
+◊; https://www.bloomberg.com/news/articles/2016-11-08/india-abolishes-inr500-1-000-rupee-notes-to-fight-corruption
+◊; https://www.cpr.org/2016/11/08/in-surprise-move-india-voids-500-and-1000-rupee-bills-to-fight-corruption/
+◊; https://www.npr.org/sections/thetwo-way/2016/11/08/501199606/in-surprise-move-india-voids-500-and-1-000-rupee-bills-to-fight-corruption?t=1567402561294
+◊; https://www.vox.com/world/2016/11/29/13763070/india-modi-cash-demonetization-protests
+◊; https://www.theguardian.com/world/2016/nov/10/rupee-note-cancellation-panic-in-india-banks-500-1000
+
+◊subhead{Civil forfeiture}
+
+◊; https://www.wsj.com/articles/SB10001424053111903480904576512253265073870
+◊; https://www.washingtonpost.com/news/opinions/wp/2014/01/15/gawker-on-asset-forfeiture-abuse-at-nypd/
+
+◊subhead{North Macedonia limits payment in cash up to 500 euros}
+
+
+◊; https://www.reddit.com/r/btc/comments/bvx83n/macedonia_bans_fiat_cash_payments_above_560/
+◊; http://www.china.org.cn/world/Off_the_Wire/2019-06/01/content_74845553.htm
+
+◊subhead{Man walks through Boston Logan Airport with $50K in a backpack; government wants to keep it}
+
+
+◊; https://www.masslive.com//boston/2019/02/man-walks-through-boston-logan-airport-with-50k-in-a-backpack-government-wants-to-keep-it.html
+
+◊subhead{Confiscating money from your bank accounts}
+
+◊; Cyprus everyone with more than €100,000 confiscated
+◊; https://www.theguardian.com/world/2013/mar/25/cyprus-bailout-deal-eu-closes-bank
+
+◊; The greek financial crisis:
+◊; https://www.econcrises.org/2017/07/20/the-greek-financial-crisis-2009-2016/
+◊; https://www.nytimes.com/interactive/2016/business/international/greece-debt-crisis-euro.html
+◊; https://en.wikipedia.org/wiki/Greek_government-debt_crisis
+
+◊subhead{Safe deposit boxes aren't safe}
+
+◊; https://news.ycombinator.com/item?id=20545276
+
+◊todo{Image with deposit boxes with the same numbers}
+
+◊subhead{Inflation}
+
+There is another way governments can take money from you: by inflating the money supply and using the excess money for themselves. It's a more indirect---and perhaps sinister---way to take money from you as you probably won't notice it. After all they aren't taking something physically from you, you still have your cash in your mattress and numbers in your bank account are unchanged, but they now buy you less stuff.
+
+◊todo{Inflation chart}
+
+Have you asked your grandmother or your great-grandmother what they used to buy when they were little?
+
+◊todo{Some cute image here}
+
+We'll go into the more extreme version of inflation, which can destabilize the whole country, in a ◊link[hyperinflation-rel]{later chapter}.
+
+◊(define hyperinflation-rel "/hyperinflation.html")
+
+◊; https://www.reddit.com/r/btc/comments/c6cfbf/let_me_tell_you_a_tale_about_ecuador_that_abused/
+
+◊subhead{What cryptocurrencies does}
+
+To summarize what cryptocurrencies can do for us if we want to protect ourselves against the different kinds of government seizures:
+
+◊ol{
+    ◊li{It allows you to hold any amount of money}
+    ◊li{If you use strong encryption, nobody can confiscate it (unless they use force on you)}
+    ◊li{You can bring your money everywhere}
+    ◊li{The government cannot invalidate your money}
+    ◊li{There is nobody who can inflate the money supply}
+}
+
