7fc0955573311975fc8595758c5f0cc986bc0240
Jonas Hietala <mail@jonashietala.se>
2019-09-15 17:54:37 +0200

    More extensions

--- extensions.html.pm
+++ extensions.html.pm
@@ -5,11 +5,21 @@
 ◊(define-meta updated "2019-09-15T11:07:31+02:00")
 ◊(define-meta uuid "c83f3bb3-7277-43b0-ad8d-ee5ea4592a87")
 
-Money is perhaps the most obvious usage of decentralized byzantine fault tolerance, which is the core making cryptocurrencies work, but there are other usages. In this section I'll highlight some of the them and we'll see that we can build other functionality on top of existing cryptocurrencies.
+Money is perhaps the most obvious usage of decentralized byzantine fault tolerance, which is the core making cryptocurrencies work, but there are other usages. In this section I'll highlight some of the them and we'll see that we can build other functionality on top of existing cryptocurrencies.◊sn{blockchain-hype}
+
+◊ndef["blockchain-hype"]{
+    Always be mindful when people use the word ◊em{blockchain}. Chances are they're referring to private blockchains or blockchains which use a consensus model with known and trusted actors. ◊link[libra]{Facebook's Libra} is such an example and ◊link[ibm-blockchain]{IBM's blockchain} is another.
+
+    Don't let the similar names fool you: consensus algorithms with known participants and those with unknown participants are ◊strong{very} different.
+}
+
+◊(define ibm-blockchain "https://www.ibm.com/blockchain")
+◊(define libra "/facebooks_libra.html")
+
 
 ◊subhead{Embedding data}
 
-The first thing we can observe is that it's possible to insert data into cryptocurrencies, essentially making the data immutable. It's not especially important exactly how, but if you're curious here are some ways:
+The first thing we can observe is that it's possible to insert data into cryptocurrencies, essentially making the data immutable. It's not important exactly how, but if you're curious here are some ways:
 
 ◊ol{
     ◊li{Miners can add coinbase data to blocks.
@@ -77,13 +87,13 @@ Bitcoin does more than just transfer coins from one address to another. What it 
 
 ◊(define bitcoin-script "https://en.bitcoin.it/wiki/Script")
 
-This is for example the script of a standard Bitcoin transaction:
+This is for example the script of a standard Bitcoin transaction (amount, destination and other data is specified elsewhere):
 
 ◊code{
     OP_DUP OP_HASH160 <pubKeyHash> OP_EQUALVERIFY OP_CHECKSIG
 }
 
-The scripting language in Bitcoin is fairly limited but the scripting language in Ethereum is much more powerful and can do more things. You can for example create games where you ◊link[cryptokitties]{buy and sell cats} on Ethereum.◊sn{tradeoffs}
+The scripting language in Bitcoin is fairly limited but the ◊link[solidity]{scripting language in Ethereum} is much more powerful and can do more things. You can for example create games where you ◊link[cryptokitties]{buy and sell cats} on Ethereum.◊sn{tradeoffs}
 
 ◊ndef["tradeoffs"]{
     Ethereum's powerful scripting language comes with all sorts of trade-offs. It makes it much more difficult to scale---which is already difficult with Bitcoin.
@@ -97,3 +107,6 @@ Now it doesn't matter if you know what a script is or how it works, just remembe
 ◊(define turing-complete "https://en.wikipedia.org/wiki/Turing_completeness")
 ◊(define ether-gas "https://blockgeeks.com/guides/ethereum-gas/")
 ◊(define multisig "https://en.bitcoin.it/wiki/Multisignature")
+◊(define bitcoin-script-tutorial "https://blockgeeks.com/guides/best-bitcoin-script-guide/")
+◊(define solidity "https://solidity.readthedocs.io/en/v0.5.11/")
+
