929d04d0b361938393716972bb2429af316c82bf
Jonas Hietala <mail@jonashietala.se>
2019-10-02 07:52:43 +0200

    Financial crisis

--- financial_crisis.html.pm
+++ financial_crisis.html.pm
@@ -13,34 +13,48 @@
   }
 }
 
-During 2007--2008 the world experienced the biggest financial crisis since the 1930s ◊link[great-depression]{Great Depression}, often simply referred to as the ◊link[2008-crisis]{2008 financial crisis}. This is an extremely condensed version of the causes and effects of the crisis and how it relates to sound money.
+During 2007--2008 the world experienced the biggest financial crisis since the 1930s ◊link[great-depression]{Great Depression}, often referred to as the ◊link[2008-crisis]{2008 financial crisis}. This chapter has an extremely condensed version of the causes and effects of the crisis and how it relates to sound money.
 
 ◊(define great-depression "https://en.wikipedia.org/wiki/Great_Depression")
 ◊(define 2008-crisis "https://en.wikipedia.org/wiki/Financial_crisis_of_2007%E2%80%932008")
 
 
-◊subhead{Background on the 2008 financial crisis}
+◊subhead{What caused the 2008 financial crisis?}
 
-The financial crisis was caused by too many people with very little money borrowing a lot of money they could never repay. The banks gladly helped them and repackaged the bad loans and sold them to someone else, for a nice profit. This eventually blew up and brought the global economy to it's knees.
+The financial crisis was caused by too many people with very little money borrowing  money they could never repay. The banks gladly helped them and repackaged the bad loans and sold them to someone else, for a nice profit. This eventually blew up and brought the global economy to it's knees.
 
 Of course this explanation is too simplified. There were for example two bubbles that popped; the housing bubble and the much bigger bond bubble. The complex financial products which appeared to be low-risk but were very high-risk. And of course the "heads I win, tails you lose" incentives at play all the way from top to bottom in the system.◊sn{villains}
 
-A lot of ink has been spent on articles, books and even movies explaining the events better and in far more detail than I could hope to do. I particularly like the explanations given in the Oscar-winning movie ◊link[the-big-short]{The Big Short (2015)}:
+A lot of ink has been spent on articles, books and movies explaining the events better and in far more detail than I could hope to do. I particularly like the explanations given in the Oscar-winning movie ◊link[the-big-short]{The Big Short (2015)}:
 
-◊youtube[margot-robbie]{
-    Margot Robbie describes subrime mortgages.
+◊qt[#:author "Margot Robbie"
+    #:src "The Big Short"
+    #:quote-src #t
+    #:url "https://www.youtube.com/watch?v=anSPG0TPf84"]{
+  Basically, Lewis Ranieri's mortgage bonds were amazingly profitable for the big banks. They made billions and billions on their 2% fee they got for selling each of these bonds. But then, they started running out of mortgages to put in them. After all, there are only so many homes and so many people with good enough jobs to buy them, right?
+
+  So, the banks started filling these bonds with riskier and riskier mortgages. That way, they can keep that profit machine churning, alright? By the way, these risky mortgages are called "subprime." So, whenever you hear the word "subprime," think "shit."
 }
 
-◊youtube[anthony-bourdain]{
-    Anthony Bourdain explains what a CDO is.
+◊qt[#:author "Anthony Bourdain"
+    #:src "The Big Short"
+    #:quote-src #t
+    #:url "https://www.youtube.com/watch?v=Vrv9odqUvlw"]{
+  OK, I'm a chef on a Sunday afternoon, setting the menu at a big restaurant. I ordered my fish on Friday, which is the mortgage bond that Michael Burry shorted. But some of the fresh fish doesn't sell. I don't know why. Maybe it just came out halibut has the intelligence of a dolphin.
+
+  So, what am I going to do? Throw all this unsold fish, which is the BBB level of the bond, in the garbage, and take the loss? No way. Being the crafty and morally onerous chef that I am, whatever crappy levels of the bond I don't sell, I throw into a seafood stew. See, it's not old fish. It's a whole new thing! And the best part is, they're eating 3-day-old halibut. ◊em{That} is a CDO.
 }
 
-Seriously if you want an approachable explanation of the crisis, watch the movie.
+◊;youtube[margot-robbie]{ Margot Robbie describes subrime mortgages.  }
+
+◊;youtube[anthony-bourdain]{Anthony Bourdain explains what a CDO is.}
+
+If you want an approachable explanation of the crisis, watch the movie. If it's too much Hollywood for your taste I can recommend "The Big Short: Inside the Doomsday Machine" by Michael Lewis, which the movie is based on.
 
 
 ◊(define the-big-short "https://www.imdb.com/title/tt1596363/")
 ◊(define margot-robbie "https://www.youtube.com/watch?v=anSPG0TPf84")
-◊(define anthony-bourdain "https://www.youtube.com/watch?v=Vrv9odqUvlw")
+◊(define anthony-bourdain "https://www.youtube.com/watch?v=kxN_qPuefrM")
 
 
 ◊ndef["villains"]{
@@ -54,15 +68,36 @@ Seriously if you want an approachable explanation of the crisis, watch the movie
     6. Overrated rating agencies
     7. Crazy compensation schemes
 
-    He does a very good job in the book in describing the background of the crisis, although he's been criticised as being too positive towards the FED and the politicians in his conclusion.
+    He does a good job describing the background of the crisis, although he's been criticised as being too positive towards the actions of the FED and the politicians.
 }
 
 
-◊subhead{How the banks were bailed out}
+◊subhead{The effects of the crisis}
+
+The crisis, which began with the collapse of the bank Lehman Brothers, issued in the ◊link[great-recession]{Great Recession}. In the U.S. alone the crisis meant $18 trillion dollars disappeared, millions of jobs were lost and more than a million people lost their homes.◊sn{music-stopped}
+
+◊ndef["music-stopped"]{
+    "After the Music Stopped" Alan S. Blinder
+}
+
+◊(define great-recession "https://en.wikipedia.org/wiki/Great_Recession")
+
+◊todo{IMG of the GDP growth rates}
+
+The crisis spread globally and for example in only the first quarter of 2009 the GDP declined by 14.4% in Germany, 15.2% in Japan and 9.8% in the Euro area. (TODO source)
+
+
+◊subhead{The bail-outs}
 
 https://www.thebalance.com/aig-bailout-cost-timeline-bonuses-causes-effects-3305693
 
-◊subhead{The aftermath, nothing really changed}
+
+◊subhead{A message from Satoshi}
+
+Right in the middle of the recession Satoshi released 
+
+
+◊subhead{Nothing really changed}
 
 But at least we're going to see some of them go to jail.
 Except they took the money the American people gave them and gave themselves huge bonuses.
@@ -73,7 +108,8 @@ Which, according to Bloomberg News, is just another name for a CDO.
 Banks Warm to Mortgage Bonds That Burned Them in 2008
 https://www.wsj.com/articles/banks-warm-to-mortgage-bonds-that-burned-them-in-2008-11568626202
 
-◊subhead{What can cryptocurrencies do?}
+
+◊subhead{What if we used sound money?}
 
 
 
