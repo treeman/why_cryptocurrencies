e2d4f57493fe03ec17a4f492e1ea3e92c4ab950c
Jonas Hietala <mail@jonashietala.se>
2019-10-30 15:05:14 +0100

    words

--- the_blind_leading_the_blind.html.pm
+++ the_blind_leading_the_blind.html.pm
@@ -16,7 +16,7 @@
 
 ◊todo{IMG of a blindfolded dude leading masses of blindfolded dudes heading towards a cliff.}
 
-One of the most common arguments against sound money---like cryptocurrencies or money backed by gold---is that you lose the ability to react to the market. For example during a depression you could try to stimulate the economy to avoid the bad effects. This lies at the heart of Keynesian economic theory where you're supposed to push on the gas pedal during harsh economic times and pull on the breaks during good times. ◊sn{elaciticity}
+One of the most common arguments against ◊link[sound-money]{◊em{sound money}}---like cryptocurrencies or money backed by gold---is you lose the ability to react to the market. For example during a depression you could try to stimulate the economy to avoid the bad effects. This lies at the heart of Keynesian economic theory where you're supposed to push on the gas pedal during harsh economic times and pull on the breaks during good times. ◊sn{elaciticity}
 
 ◊ndef["elaciticity"]{
     ◊em{Elaciticity} is a nice word fractional banking proponents have used to describe this ability. In plain English it means the ability to inflate and deflate the money supply freely.
@@ -27,13 +27,15 @@ But there is a fatal flaw with this argument: it relies on being able to predict
 
 ◊subhead{How to become an expert}
 
-To become an expert in a field of course you need a lot of dedication, effort and maybe a bit of talent.◊sn{10000h} But as Daniel Kahneman points out in his book "Thinking, Fast and Slow" you also need the right environment to develop expertise:
+To become an expert in a field you need a lot of dedication, effort and maybe a bit of talent,◊sn{10000h} but as Daniel Kahneman points out in his book “◊link[thinking-fast-and-slow-book]{Thinking, Fast and Slow}” you also need the right environment to develop expertise:
 
 ◊ndef["10000h"]{
-    There's a misunderstood myth that you only need 10,000 hours, or that you need at least 10,000 hours before you become an expert. But that's not the truth, with the right environment and ◊em{dedicated practice} you can become an expert much faster and if you use your time inefficiently you may never become one.
+    There's a misunderstood myth that you only need 10,000 hours, or that you need at least 10,000 hours before you become an expert. But that's not the truth, with the right environment and ◊link[deliberate-practice]{◊em{deliberate practice}} you can become an expert much faster and if you use your time inefficiently you may never become one.
 
     Think for example of the teenage golf prodigies who eclipse the middle-aged men---who've spent their entire lives playing golf---in no time at all.
 }
+
+◊(define deliberate-practice "https://expertenough.com/1423/deliberate-practice")
 
 ◊ul{
     ◊li{An environment that is sufficiently regular to be predictable.}
@@ -49,25 +51,21 @@ For example picking stock is notoriously difficult, so difficult that in general
 In contrast learning how to drive a car provides an ideal learning environment. You'll get instant feedback whenever you break or use the gas pedal, when you turn the steering-wheel the car turns and you'll quickly find out if you changed gears correctly. While similar learning how to maneuver a large ship is more difficult because of the delayed feedback.
 
 
-
-
-
 ◊subhead{Why planning economy doesn't work}
 
 ◊todo{IMG of a butterfly flapping it's wings causing a tornado on the other side}
 
 
+
+
 ◊subhead{Our leaders are blind}
 
-Because the economy on a global scale is so incredibly complex it's extremely hard, some would say impossible, to accurately predict how it'll behave. Just look at the ◊link[financial-crisis]{financial crisis in 2008} which in hindsight should've been obvious, but it took almost everyone by surprise.◊sn{black-swan}
-
-◊ndef["black-swan"]{
-    The 2008 crisis is an example of a ◊em{black swan} event. Impossible to predict before it happens but obvious in hindsight. Coincidentally the meteoric rise of Bitcoin can also be considered a black swan.
+◊qt[#:author "Citigroup CEO Chuck Prince"
+    #:date "2007"]{
+  When the music stops, in terms of liquidity, things will be complicated. But as long as the music is playing, you’ve got to get up and dance. We’re still dancing.
 }
 
-◊(define financial-crisis "/financial_crisis.html")
-
-Here are some quotes from some of the leading experts of the world, merely months before the financial meltdown:
+It's not just the planning economy of the Soviet Union that has problems predicting and adapting to the whims of the global economy. Look at the ◊link[financial-crisis]{financial crisis in 2008} which should've been obvious, but took almost everyone by surprise. Here's what the Federal Reserve had to say, merely months before the financial meltdown:
 
 ◊qt[#:author "The Federal Reserve"
     #:date "August, 2007"
@@ -77,16 +75,24 @@ Here are some quotes from some of the leading experts of the world, merely month
   Nevertheless, the economy seems likely to continue to expand at a moderate pace over coming quarters, supported by solid growth in employment and incomes and a robust global economy.
 }
 
-◊qt[#:author "Citigroup CEO Chuck Prince"
-    #:date "2007"]{
-  When the music stops, in terms of liquidity, things will be complicated. But as long as the music is playing, you’ve got to get up and dance. We’re still dancing.
+The Federal Reserve, calling the global economy "robust", fanned the flames of the housing bubble by focusing on inflation and keeping the rent low, all the way up to the crash. They also let Lehman Brothers fail, not realizing the realizing the disaster it would create.◊sn{music-stops}
+
+Maybe you feel like I'm focusing too much on the ◊link[financial-crisis]{2008 financial crisis}? Then how about their actions during the ◊link[great-depression]{Great Depression}, of which the Federal Reserve chairman had this to say:
+
+◊qt[#:author "Ben S. Bernanke, chairman of the Federal Reserve"
+    #:date "2002"
+    #:url "https://www.federalreserve.gov/boarddocs/speeches/2002/20021108/default.htm"]{
+    Let me end my talk by abusing slightly my status as an official representative of the Federal Reserve. I would like to say to Milton and Anna: Regarding the Great Depression, you're right. We did it. We're very sorry. But thanks to you, we won't do it again.
 }
 
-The Federal Reserve fanned the flames of the housing bubble by focusing on inflation and keeping the rent low, all the way up to the crash. During the crisis the Fed bailed-out Bear Stearns but let Lehman Brothers fail, which has been widely criticized afterwards.◊sn{music-stops}
+
+◊(define financial-crisis "/financial_crisis.html")
 
 ◊ndef["music-stops"]{
-    Alan S. Blinder "After the Music Stopped".
+    “◊link[after-the-music-stopped-book]{After the Music Stopped}” Alan S. Blinder
 }
+
+This shouldn't be that surprising when you consider that the national and global economy is too irregular to be predictable, and the feedback loop is extremely long, if it at all exists. If you for example want to lower the central bank's interest rate, how long do you have to wait to see the result? Days, months maybe even years. And how do you know the economy reacted because of your changes to the interest rate, and not because of the trade war between the U.S. and China, brexit or a million other reasons?
 
 ◊;https://news.ycombinator.com/item?id=21028770
 
@@ -129,3 +135,4 @@ The Fed is not a government agency. It's a private entity and its shareholders a
     In this system, our system, the slaves are unaware, both of their status and of their masters, who exist in a world apart and where the shackles are hidden amidst reams of unreachable legalese.
 }
 
+◊(define sound-money "https://www.soundmoneydefense.org/sound-money-explained")
