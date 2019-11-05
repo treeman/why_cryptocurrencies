f559d3cf05e360cd944ed2dc2e313371f13cfca8
Jonas Hietala <mail@jonashietala.se>
2019-11-05 08:22:29 +0100

    Polish

--- about_me.html.pm
+++ about_me.html.pm
@@ -1,24 +1,41 @@
 #lang pollen
 
 ◊(define-meta title "About me, the author")
-◊(define-meta updated "2019-04-03T19:59:38+02:00")
+◊(define-meta subtitle "Hello, I'm Jonas")
+◊(define-meta updated "2019-11-05T07:54:33+01:00")
 ◊(define-meta uuid "b1aaa891-a7c6-4bf4-a01c-4c43c0446abf")
 
-I wonder what you might think of someone who's writing a book about cryptocurrencies? Maybe you'll think of a teenage multi-millionaire or an old neckbeard raving about free software? Or perhaps an anarchist using slogans like "governments are evil" and "taxation is theft"?
+I wonder what you might think of someone who's writing a book about cryptocurrencies? Maybe you'll think of a teenage multi-millionaire or an old neckbeard raving about free software? Or perhaps an anarchist saying things like "governments are evil" and "taxation is theft"?◊mn{there-are}
+
+◊ndef["there-are"]{
+    It's easy to see why cryptocurrencies might attract people like this. Cryptocurrencies are all about freedom, and the free software movement (saying that ◊strong{all} software should be free) and anarchism seek freedom to the extreme.
+}
 
 But I am none of that. I'm just a normal person, perhaps a bit introverted but still. I only have a short beard and I pay my taxes and enjoy the benefits we get here in Sweden. Unfortunately I'm neither a teenager nor a multi-millionaire.
 
-I first heard about bitcoin in 2010, about one year after it's creation. I installed a wallet and played around with it a little. It was just another internet thing for me and like many other it failed to hold my attention. After about 10 or 15 minutes I removed it and promptly forgot about it (no, I've lost all keys from that time).
+I first heard about bitcoin in 2010, about one year after it's creation. I installed a Bitcoin wallet and played around with it a little. It was just another internet thing for me and like many others it failed to hold my attention. After about 10 or 15 minutes I removed it and promptly forgot about it.◊sn{had-some}
 
-It wasn't until four or maybe five years later I looked at Bitcoin again. I studied computer science and we had a course in cryptography, a very fun course I might add, where we had a section about cryptocurrencies. We went through the technical details and it made me curious about Bitcoin on a higher level. I read the white paper and began follow the news and development in the space.
+◊ndef["had-some"]{
+    I did have a couple of bitcoins at that time. It hurts to admit this as today a single bitcoin is worth around $9,000, but sadly I've lost them. After all, back then they weren't worth anything.
+}
 
-I'm still at the point where I'm not actively involved in cryptocurrencies. I hang out on Reddit, follow the development and I own a little but I'm not writing code or working for a project. While I am a software developer I don't have a cryptocurrency project I burn for enough to work on it on my free time. Unfortunately ever since I started programming full-time I just don't have energy for it, like I had during my University years.
+It wasn't until four or maybe five years later I looked at Bitcoin again. I studied computer science and we had a course in cryptography---a very fun course I might add---with a section about cryptocurrencies. We went through the technical details and it made me curious about Bitcoin on a higher level. I read the white paper and began follow the news and development in the space.
 
-The free time I do have has been taken up by my 1.5 year old boy, my girlfriend and other hobbies. Saying no to something is saying yes to something else.
+I'm still at the point where I'm not actively involved in cryptocurrencies. I hang out on Reddit, follow the development and I own a little but I'm not writing code or working on a project. While I am a software developer I don't have a cryptocurrency project I burn for enough to work on it on my free time. Unfortunately ever since I started programming full-time it's been difficult to find energy for side-projects, like I had during my University years.
 
-After my parental leave we decided that I should work part time to avoid long days at pre-school.  Working less has also opened up time and energy for me to work on a side project. Writing a book has been on my bucket-list so why not do it now? My girlfriend has always been super supportive of my silly ideas.
+The free time I do have has been taken up by my 2 year old boy, my girlfriend and other hobbies. Saying no to something is saying yes to something else.◊sn{hobbies}
+
+◊ndef["hobbies"]{
+    My current hobbies include ◊link[bjj]{Brazilian Jiu-Jitsu}, boardgames and standard low-effort ones such as reading books or watching videos.
+
+    I do have a very on-and-off relationship with my hobbies, which I pick up and do intensely for a short period of time. For instance I've been into ◊link[lockpicking]{lockpicking}, ◊link[go]{Go} and learning Korean, which I would like to pick up again some day.
+}
+
+After my parental leave we decided that I should work part time to avoid long days at pre-school.  Working less has also opened up time and energy for me to work on a side project, and writing a book has been on my bucket-list so why not do it now? My girlfriend has always been super supportive of my silly ideas.
 
 I have a ◊link[blog]{blog} where I write about random stuff, mostly for myself.
 
 ◊(define blog `("http://www.jonashietala.se/" "My personal blog"))
-
+◊(define bjj "https://www.grapplearts.com/japanese-jiujitsu-vs-bjj/")
+◊(define lockpicking "https://www.reddit.com/r/lockpicking/")
+◊(define go "https://en.wikipedia.org/wiki/Go_(game)")
