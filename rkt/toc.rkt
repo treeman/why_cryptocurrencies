01c22398c88e07b84401589f853a1b3f29d73945
Jonas Hietala <mail@jonashietala.se>
2019-11-11 06:53:04 +0100

    Hide a defective system

--- rkt/toc.rkt
+++ rkt/toc.rkt
@@ -25,7 +25,7 @@
     (better_currency.html
      financial_crisis.html
      the_blind_leading_the_blind.html
-     a_defective_system.html
+     "A defective system"
      "Anonymous Money"
      "A global currency")
     ("Brave new world"
