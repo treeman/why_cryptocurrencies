7ddd33b64b43bb0092de580fc5b6a5428439e799
Jonas Hietala <mail@jonashietala.se>
2019-11-14 15:28:21 +0100

    Words

--- global_currency.html.pm
+++ global_currency.html.pm
@@ -5,4 +5,40 @@
 ◊(define-meta updated "2019-09-02T07:02:25+02:00")
 ◊(define-meta uuid "bec2e177-8ccd-4f8d-a90e-a3d0b4269e43")
 
-TODO
+◊subhead{A world currency}
+
+JPMorgan: We Believe The Dollar Could Lose Its Status As World's Reserve Currency
+◊link{https://www.zerohedge.com/news/2019-07-23/jpmorgan-we-believe-dollar-could-lose-its-status-worlds-reserve-currency}
+
+I wanted to help someone in #Venezuela , so I'm tutoring a novice #JavaScript developer as he writes #opensource software. I pay him $100 per week. It's making a huge difference in his life. With #BitcoinCash we don't have to consider what governments or banks think about it.
+◊link{https://mobile.twitter.com/christroutner/status/1142080363981709313}
+◊link{https://www.reddit.com/r/btc/comments/c3brvi/chris_troutner_i_wanted_to_help_someone_in/}
+
+
+◊subhead{Remittance}
+
+Compare the fees and speed of Bitcoin Cash and Western Union.
+
+The charity EatBCH feeds Venezuelans.  Sending money to an inaccessible country using cryptocurrencies.
+◊link{https://twitter.com/eatBCH}
+◊link{https://news.bitcoin.com/meet-the-charity-eat-bch-the-p2p-electronic-cash-to-food-system/}
+
+
+◊subhead{Capital flight}
+
+"I Don't Want My Money Trapped Here" - Hong Kong Hit By Capital Exodus As Protests Drag On
+◊link{https://www.zerohedge.com/news/2019-08-18/i-dont-want-my-money-trapped-here-hong-kong-hit-capital-exodus-protests-drag}
+
+
+◊subhead{A safe haven}
+
+Why are Venezuelans seeking refuge in cryptos?
+◊link{https://www.bbc.com/news/business-47553048}
+
+Protection against hyperinflation, see for example Venezuela and Zimbabwe.
+
+Historically how many currencies have gone under to hyperinflation?
+
+◊link{https://cryptoslate.com/venezuelan-crisis-cryptocurrency-privacy/}
+◊link{https://www.nytimes.com/2019/02/23/opinion/sunday/venezuela-bitcoin-inflation-cryptocurrencies.html}
+
