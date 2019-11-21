42eadd8021c72d8cfa830bd620f73121ce2f4900
Jonas Hietala <mail@jonashietala.se>
2019-11-21 15:54:27 +0100

    words

--- properties_of_a_cryptocurrency.html.pm
+++ properties_of_a_cryptocurrency.html.pm
@@ -2,7 +2,7 @@
 
 ◊(define-meta title "Properties of a cryptocurrency")
 ◊(define-meta subtitle "Trustless and permissionless")
-◊(define-meta updated "2019-11-05T08:20:50+01:00")
+◊(define-meta updated "2019-11-21T15:38:06+01:00")
 ◊(define-meta uuid "176f3433-baba-49d6-ab43-9c08061f0682")
 
 ◊epigraph{
@@ -138,26 +138,28 @@ Either way it's not an argument against cryptocurrencies in general as they can 
 
 ◊subhead{Anonymous}
 
-Bitcoin uses a public ledger where all transactions are public---it isn't anonymous. It's ◊em{pseudo-anonymous}, while you can see transactions and addresses you don't know who owns an address. But you know someones address and can then trace all past and future transactions moving through that address. You can use a ◊link[blockchair]{blockchain explorer} to see for yourself.
+Bitcoin, and most cryptocurrencies, use a public ledger where all transactions are public. They're not anonymous---they're ◊em{pseudo-anonymous}: while you can see all transactions and addresses you don't know who owns an address. But if you know someone's address, for example they sent money to you, you can then trace all past and future transactions moving through that address.
+
+◊;But you know someones address and can then trace all past and future transactions moving through that address. You can use a ◊link[blockchair]{blockchain explorer} to see for yourself.
 
 ◊todo{Rewrite and use an image which explains this}
 
-There are cryptocurrencies that tries to hide this information.◊sn{monero} On them you cannot trace transactions on the blockchain and since these projects exists I will call cryptocurrencies anonymous, just keep in mind not all are.
+There are cryptocurrencies that tries to hide this information meaning you cannot trace transactions on the blockchain.◊sn{monero} Therefore I'll call cryptocurrencies anonymous, just keep in mind not all are.
 
 ◊ndef["monero"]{
     ◊link[Monero]{Monero} is perhaps the most well known cryptocurrency that hides inputs, outputs and amounts for all transactions by default.
 
-    There are others where you can opt-in to hide some transactions but otherwise they can be traced (like the shielded transactions of ◊link[zcash]{ZCash}). This also exists as a service on top of Bitcoin, see for example ◊link[coinjoin]{CoinJoin}, although they're not as good.
+    There are others where you can opt-in to hide some transactions but otherwise they can be traced (like the shielded transactions of ◊link[zcash]{ZCash}). This also exists as services on top of Bitcoin, see for example ◊link[coinjoin]{CoinJoin}, although they're not as good.
 }
 
-As with all privacy tools, it won't ◊em{guarantee} to keep you anonymous. There are always trade-offs and weaknesses and nothing is perfect.
+As with all privacy tools, using cryptocurrencies won't ◊em{guarantee} your anonymity. There are always trade-offs and weaknesses---nothing is perfect.
 
 
 ◊subhead{Large and small amounts behave the same}
 
 In contrast to cash or gold where large amounts can be cumbersome to handle there's no difference between large or small transfers in a cryptocurrency. Transaction costs are the same for small transfers as for large transfers and they are just as secure. Wallets can store as much as you're comfortable with.
 
-It's also easy to split coins into small parts.◊sn{satoshis} In fact you don't have to think about dividing at all---you use a wallet just like a credit card---a transfer is always exact.
+It's also easy to split coins into small parts.◊sn{satoshis} In fact you don't have to think about dividing at all, you use a wallet just like a credit card---a transfer is always exact.
 
 ◊ndef["satoshis"]{
     For example one bitcoin can be divided into one million parts---called satoshis. But there is no real technical limit, only a usability concern.
