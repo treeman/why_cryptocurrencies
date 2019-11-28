684fe916adb6fbdaff7d666bd4ede95c64527d3e
Jonas Hietala <mail@jonashietala.se>
2019-11-28 22:30:11 +0100

    More words

--- anonymous_money.html.pm
+++ anonymous_money.html.pm
@@ -200,7 +200,76 @@ Even the constitution of the United States---written more than a hundred years b
 }
 
 
-◊subhead{We're living in Stasi's wet dream}
+◊subhead{We're living in a Stasi fantasy}
+
+The ◊link[stasi]{Stasi}, the secret police of East Germany, has ◊link[stasi-repression]{been described} as one of the most repressive organizations in the world. One of their purposes was to find and imprison political enemies, which basically meant anyone critical of the government.
+
+They used 100,000 employees and between 500,000 to 2,000,000 snitches to maintain files on more than one-third of the population. There were always spies in the bars and libraries who listened to your conversations and they encouraged (or threatened) people to snitch on their co-workers, neighbours and family.◊sn{threats}
+
+◊ndef["threats"]{
+    People could be placed in prison and tortured by isolation or sleep deprivation in the hopes of getting them to snitch on others.
+}
+
+◊qt[#:author "Edward Snowden"
+    #:src "Permanent Record"
+    #:quote-src #t
+    #:url permanent-record-book]{
+  In an authoritative state, rights derive from the state and are granted to people. In a free state, rights derive form people and are granted to the state.
+}
+
+The goal of the Stasi was to imprison everyone critical of the regime. To do that they tried to map out the lives of everyone---where they went, what they did, what they said and who they talked to. This was painstakingly difficult and required tremendous resources.
+
+But today we live in a society the Stasi couldn't ever hope for. There's no need for a shady person to follow us around, because we already have a surveillance device in our pocket. All information they could ever want is already available, they just have reach out and grab it.
+
+Back in 2013 Edward Snowden revealed that governments were doing just this.
+
+Here are just ◊link[snowden-leaks]{some of the things} he leaked:
+
+◊ol{
+    ◊li{Listen to all phone calls
+
+        The N.S.A. (the U.S. intelligence service) collect all U.S. phone calls and text messages. They also collect foreign phone calls, including those of foreign leaders such as Angela Merkel.
+    }
+    ◊li{Remotely controlled phones
+
+        The GCHQ (the british intelligence service) can remotely control your phone. They can for example turn it on remotely and use the microphone to record you.
+    }
+    ◊li{Big tech data
+
+        The government can ask for (or rather demand) data from the big tech companies like Facebook, Google or Apple. If they refuse they can just hack them and steal the data anyway.◊sn{nsa-legality}
+
+        ◊ndef["nsa-legality"]{
+            The N.S.A. has proved again and again that they stand above such trivial things as legality. For example James Clapper, the Director of National Intelligence, ◊link[clapper]{lied under oath to the Senate}, without any consequence.
+
+            If I did that I'd be in prison for years.
+        }
+    }
+    ◊li{A search of you
+
+        There's an internal search engine where N.S.A. employees can search through your emails, pictures, medical records and private Facebook messages. Like Google, but with the contents of your online life.
+
+        Snowden writes that it was common for N.S.A. employees to abuse this to spy on their girlfriends, ex-girlfriends or girls they were interested in. This includes sharing nude photos with their co-workers, which the girls either took themselves or photos the N.S.A. took by hacking their webcam. (Quite literally a stalker's wet dream.)◊sn{no-good-guys}
+
+        ◊ndef["no-good-guys"]{
+            This is a counterpoint to the argument that it's fine for the government or the police to have this data. The government or the police aren't full of only good people---there will always be bad ones somewhere.
+        }
+    }
+}
+
+As is praxis for authoritative regimes, this capability isn't used to protect the people of the state, but the state itself. Snowden's leaks were to the benefit of the people, yet he and other whistleblowers like him are ◊link[full-power]{facing the full power of the U.S. aimed at them}.
+
+
+◊(define snowden-lawsuit "https://news.bitcoin.com/snowden-us-seizing-my-book-revenue-is-good-for-bitcoin/")
+◊(define full-power "https://theintercept.com/2019/08/04/whistleblowers-surveillance-fbi-trump/")
+
+
+◊;link{https://mashable.com/2014/06/05/edward-snowden-revelations/?europe=true}
+◊;link{https://prefuse.org/edward-snowden-leaks#the-nsa-can-demand-call-records-and-data-from-telephone-companies}
+
+◊(define clapper "https://www.youtube.com/watch?v=AGYn7ER5U_0")
+◊(define stasi "https://www.britannica.com/topic/Stasi")
+◊(define stasi-repression "https://www.reuters.com/article/us-germany-wall-stasi/no-remorse-from-stasi-as-berlin-marks-fall-of-wall-idUSL118487020091104")
+◊(define snowden-leaks "https://prefuse.org/edward-snowden-leaks#the-nsa-can-demand-call-records-and-data-from-telephone-companies")
 
 ◊qt[#:author "Edward Snowden"
     #:src "Permanent Record"
@@ -209,21 +278,22 @@ Even the constitution of the United States---written more than a hundred years b
   Once the ubiquity of collection was combined with the permanency of storage, all any government had to do was select a person or a group to scapegoat and go searching---as I'd gone searching through the agency's files---for evidence of a suitable crime.
 }
 
-◊qt[#:author "Edward Snowden"
-    #:src "Permanent Record"
-    #:quote-src #t
-    #:url permanent-record-book]{
-  In an authorative state, rights derive from the state and are granted to people. In a free state, rights derive form people and are granted to the state.
+Snowden now lives in exile in Russia, where the black bags of the U.S. cannot reach. He recently released his memoir “◊link[permanent-record-book]{Permanent Record}” which details his life leading up to the leaks. It's a great book, but the U.S. doesn't want you to read it. They even ◊link[snowden-lawsuit]{filed a lawsuit against him} and seized the revenue of the book.◊sn{post-lawsuit}
+
+◊ndef["post-lawsuit"]{
+    As in response to the lawsuit his book shot up to the Amazon bestseller list. Snowden concluded that “◊link[snowden-good-for-bitcoin]{this is good for bitcoin}”.
 }
 
-Administration Using the Full Power of the Surveillance State on Whistleblowers
-◊link{https://news.ycombinator.com/item?id=20615084}
 
-◊link{https://news.bitcoin.com/snowden-us-seizing-my-book-revenue-is-good-for-bitcoin/}
+◊;Administration Using the Full Power of the Surveillance State on Whistleblowers
+◊;◊link{https://news.ycombinator.com/item?id=20615084}
+◊;
+◊;◊link{https://news.bitcoin.com/snowden-us-seizing-my-book-revenue-is-good-for-bitcoin/}
 
-Supporting finanical privacy makes me an extremist
-◊link{https://www.coingecko.com/buzz/erik-voorhees-supporting-financial-privacy-extremist}
+◊;Supporting finanical privacy makes me an extremist
+◊;◊link{https://www.coingecko.com/buzz/erik-voorhees-supporting-financial-privacy-extremist}
 
+◊(define snowden-good-for-bitcoin "https://twitter.com/snowden/status/1174090027648868353?s=21")
 
 ◊subhead{Fear is the mind-killer}
 
@@ -242,7 +312,7 @@ I think people often give up their privacy because of ◊strong{fear}. Since the
 
 Terrorism is real and scary, there's no denying that. But our level of fear is irrational and would be better placed worrying about cars or unhealthy food---both of which kill many more people than terrorism do. Yet fear is such a powerful emotion that it prevents us from thinking logically.
 
-For example people may be willing to give up their privacy, because it might make it easier to catch terrorists. But if we try to compromise everyones' privacy, we'll jeopardize the personal security of the good guys, and the bad guys will still have access to privacy through strong encryption.◊sn{strong-encryption}
+For example people may be willing to give up their privacy, because it might make it easier to catch terrorists. But if we try to compromise everyones' privacy, we'll jeopardize the personal security of the innocent, while the terrorists will still have access to privacy through strong encryption.◊sn{strong-encryption}
 
 ◊ndef["strong-encryption"]{
     With strong encryption it's impossible for ◊em{anyone} to read what you write or access your data.
@@ -255,13 +325,13 @@ For example people may be willing to give up their privacy, because it might mak
 ◊(define 9-11 "https://en.wikipedia.org/wiki/September_11_attacks")
 
 
-◊subhead{We're the product}
+◊subhead{You're the product}
 
 It's not just the governments of the world who are collecting our data and violating our privacy. Companies of all sorts do this too, but they sell it for profit.
 
 Alphabet Inc. (company name of Google) and Facebook are two of the most valuable, richest and powerful companies in the world. Yet their main services---search and social networking---are completely free for us to use. So where do they make their money? And where's the product they're selling?
 
-The product they're selling is you.
+The product they're selling is ◊strong{you}.
 
 They're selling knowledge about you. What you're searching for, what sites you visit, what products you buy, what places you visit, what friends you have and what you're talking about with your friends. With the combination of massive data collection and clever computer algorithms it's scary what they know about you.◊sn{predictions}
 
@@ -272,12 +342,14 @@ They're selling knowledge about you. What you're searching for, what sites you v
 }
 
 
-For example they probably know if you're gay, if you're pregnant (also if it was an accident, if your parents know about it or who's the father). They might also predict, with high confidence, if you're religious---even if you've never admitted it. Or who you're going to vote for in the ◊em{next} election or how likely you are to commit a crime.◊sn{thoughtcrime}
+For example they probably know if you're gay or if you're pregnant (and if you are, they'll know if it was an accident, if your parents know about it and who's the father). They might also predict---with high confidence---if you're religious, even if you don't want anyone to know. Or who you're going to vote for in the ◊em{next} election or how likely you are to commit a crime.◊sn{thoughtcrime}
 
 ◊ndef["thoughtcrime"]{
-    Pre-crime, or thought crime is a common theme in dystopian fantasies, which I have a morbid fascination for.
+    ◊em{Pre-crime}, like in the movie Minority Report, is the idea that you can predict someone committing a crime and catching them before they do.
 
-    Robocop, (Tom Cruise film), 1984
+    ◊em{Thoughtcrime}, as explored in the book 1984, says that thoughts are criminal. Like entertaining a politically unacceptable thought.
+
+    While fiction, our technology is edging ever closer. Soon we might have killer robots going around streets enforcing laws on our predicted behaviour. (The U.S. already deploys killing drones, which cause civilian casualties.)
 }
 
 And it's not just a problem with Google and Facebook. An increasing number of companies are discovering how lucrative your data is. If you buy a Coca-Cola, the information that you bought it at this place and at this specific time might even be worth more than the Coca-Cola itself!
@@ -291,9 +363,6 @@ For example smart TVs come with a microphone that listens to everything you do, 
 
 ◊;Credit cards have a privacy problem
 ◊;link{https://news.ycombinator.com/item?id=20800115}
-
-
-
 
 
 ◊subhead{Reclaiming our financial privacy}
@@ -332,13 +401,3 @@ So far we've had to surrender our privacy to gain access to digital payments and
 ◊(define anon "/properties_of_a_cryptocurrency.html#anonymous")
 ◊(define properties "/what_is_money.html#what-properties-does-good-money-have?")
 
-◊subhead{Remove?}
-
-Fear also forces people to be as unassuming as possible---to avoid trouble. When ◊em{everything} you do is recorded, you must assume that you're always being watched.
-
-◊qt[#:author "George Orwell"
-    #:src "1984"
-    #:quote-src #t
-    #:url 1984-book]{
-  There was of course no way of knowing whether you were being watched at any given moment. How often, or on what system, the Thought Police plugged in on any individual wire was guesswork. It was even conceivable that they watched everybody all the time.
-}
