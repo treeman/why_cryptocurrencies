6110cea20443a11f167e40c7cc9930679f468af2
Jonas Hietala <mail@jonashietala.se>
2019-12-12 13:45:21 +0100

    Words

--- swiss_bank_account_in_your_pocket.html.pm
+++ swiss_bank_account_in_your_pocket.html.pm
@@ -12,15 +12,34 @@
     ◊li{Cryptocurrencies is an actual swiss bank account in your pocket}
 }
 
+◊; https://www.swissinfo.ch/eng/automatic-exchange-_switzerland-grants-18-more-countries-access-to-bank-details/45424544
 
-◊subhead{The Panama Papers}
 
-Maybe you'll say this is extremely unwanted as it would enable tax-fraud on a global scale. And you'd have a point, there's just one issue: it's already possible.
+◊subhead{Offshore bank accounts}
+
+Maybe you'll say this is extremely unwanted as it would enable tax-fraud on a global scale. And you'd have a point, but there's just one issue: it's already possible. And it's happening at a big scale.
 
 ◊(define panama-papers "https://www.icij.org/investigations/panama-papers/")
 ◊(define panama-papers-doc "https://www.panamapapersdoc.com/")
 
+◊ol{
+    ◊li{Panama papers}
+    ◊li{Paradise papers}
+    ◊li{Bahama's secrets}
+}
+
 ◊; https://en.wikipedia.org/wiki/Panama_Papers
+
+Offshore bank accounts aren't just for tax evasion though. Celebrities have tabloids dedicated to blow up any minor detail of their life into this huge scandal. It just makes sense for them to try to keep their finances hidden, which is exactly what a spokesman for Emma Watson, who got named in the Panama Papers, claimed:
+
+◊qt[#:url "https://www.telegraph.co.uk/news/2016/05/10/panama-papers-emma-watson-named-in-latest-data-release/"
+    #:src "Spokesman for Emma Watson"]{
+  Emma receives absolutely no tax or monetary advantages from this offshore company whatsoever, only privacy.
+}
+
+Do you believe them? I'm skeptical, but at the same time I sympathize with the privacy concern---I did after all dedicate ◊link[private-money]{an earlier chapter} to this very topic.
+
+◊(define private-money "/private_money.html")
 
 
 ◊subhead{Inequality}
@@ -33,16 +52,32 @@ Let's see what the president of the United States has to say about tax evasion:
     Trump: That makes me smart.
 }
 
-No Mr. President, that makes you rich. You see, this kind of tax evasion is only available for the very rich. And it's not the 1%, more like the 1% of the 1%. We're talking about people with private chefs and who travels the world in private jets and yachts. This is why you don't see the names of normal people in the Panama Papers, but instead we'll find names like Lionel Messi, Donald Trump and Vladimir Putin.◊sn{names}
+No Mr. President, that makes you rich. You see, this kind of tax evasion is only available for the very rich.◊sn{crap-smart-rich} And it's not the 1%, more like the 1% of the 1%. We're talking about people who can afford private chefs and travel the world in private jets and yachts.
+
+◊ndef["crap-smart-rich"]{
+    And please don't gimme that crap about how all rich people are smart. It's just what rich people tell themselves, to make them feel smart. Most were born with a golden spoon in their mouth, and even the self-made ultra rich had a ton of luck helping them.
+}
+
+This is why you don't see the names of normal people in the Panama Papers, no matter how smart they may be, but instead we find names like Lionel Messi, Donald Trump and Vladimir Putin.◊sn{names}
 
 ◊ndef["names"]{
-    It's funny because you can find Trump's name in the Panama Papers 3,450 times, while ◊link[putin-proxies]{Putin uses proxies} to try and hide his involvement. Does that make Putin smarter than Trump?
+    It's funny that you can find Trump's name in the Panama Papers 3,450 times, while ◊link[putin-proxies]{Putin uses proxies} to try and hide his involvement. I wonder, does that make Putin smarter than Trump?
 }
+
+While offshore banking might be problematic, it pales to the problem that only the very rich can abuse them in this way. They get away without paying taxes, while normal people have to pay the tax man or face prison. It's especially egregious since they have so much money they can't even spend it all, but normal people have to struggle to make ends meet.
+
+The world is just extremely unfair, and if anything it should be the other way around. The poor should avoid taxes, while the ultra rich should pay up. At least this would have a semblance of fairness.
+
+◊;In 2018, 60 of the largest companies in the USA paid no taxes on pre-tax income of 79 billion dollars. (The Laundromat)
+
 
 ◊(define db "https://offshoreleaks.icij.org/")
 ◊(define putin-proxies "https://www.icij.org/investigations/panama-papers/20160403-putin-russia-offshore-network/")
 
+
 ◊subhead{The great leveler}
+
+With cryptocurrencies everyone can
 
 ◊; See db:
 ◊; https://offshoreleaks.icij.org/
