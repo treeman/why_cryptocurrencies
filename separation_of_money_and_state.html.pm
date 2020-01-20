3f605f6586489dccd19d22e8cdf53c8027d406f7
Jonas Hietala <mail@jonashietala.se>
2020-01-20 08:10:48 +0100

    Words

--- separation_of_money_and_state.html.pm
+++ separation_of_money_and_state.html.pm
@@ -11,11 +11,18 @@ When I first started thinking about cryptocurrencies I thought it was just usefu
     I do think ◊link[cheaper-faster]{avoiding 1--3%} fees on practically all digital payments is an under-appreciated benefit of cryptocurrencies. It might be overshadowed by the volatility, but the potential benefit to the economy is absolutely huge.
 }
 
-But one idea made me go "Wow, that would be absolutely huge!" And that idea was how cryptocurrencies for the first time in history make it possible for money to be separated from the state.
+But one idea made me go "Wow, that could be massive!" And that's how cryptocurrencies for the first time in modern history make it possible for money to be completely separated from the state and other institutions.
+
+◊todo{This isn't really true}
 
 ◊(define cheaper-faster "/cheaper_faster.html")
 
+
 ◊subhead{Religion and state}
+
+I know it might be difficult to entertain the notion that a country shouldn't control their own money, and that the mere suggestion is outrageous. Here I find the parallel to the relationship between religion and state relevant.
+
+For many centuries, millennia even, religion and the state were inseparable. I'm not even sure it was a conscious decision, rather the mere idea that it could be any other way was simply unthinkable. For many that's just how it was, like how the sun rose every morning or that you got hungry without food.
 
 ◊;Sweden 1 Jan 2000 formally separated the church from the state.
 ◊;https://www.dagen.se/dokument/sa-fri-ar-svenska-kyrkan-fran-staten-egentligen-1.1632522
@@ -23,6 +30,23 @@ But one idea made me go "Wow, that would be absolutely huge!" And that idea was 
 ◊;https://en.wikipedia.org/wiki/Gustav_I_of_Sweden
 ◊;https://www.britannica.com/biography/Gustav-I-Vasa
 
-◊subhead{Not that outrageous}
+
+◊subhead{Not even that unusual}
+
+In practice some countries already have separated money from their state, as they use money someone else control. For example the ◊link[euro]{Euro} is managed by the ◊link[ecb]{European Central Bank (ECB)}, and the countries that use the Euro have very little influence in the ECB's decision-making (well, most of them anyway). There are also countries that ◊link[countries-use-us]{use the U.S. dollar as their official currency}, while having no say in what the U.S. does with it.
+
+Gold might also be an example of money outside of state control---there's nobody with exclusive access to gold after all. But in practice someone has to convert the gold to coins for it to be usable as money, and that includes a "government approved" stamp on them, which gives some sort of control over the money. After all, it's difficult to determine what a coin is made of so we just trust the stamp on it.
+
+Maybe a better example is shell money, or other money found in nature, where you just pick up them up and start using them.
+
+So the idea that a state doesn't have to have control over their own money isn't really that strange, as there are many examples of the contrary.
+
 
 ◊subhead{What does the future hold?}
+
+
+
+◊(define ecb "https://en.wikipedia.org/wiki/European_Central_Bank")
+◊(define euro "https://en.wikipedia.org/wiki/Euro")
+◊(define countries-use-us "https://www.investopedia.com/articles/forex/040915/countries-use-us-dollar.asp")
+
