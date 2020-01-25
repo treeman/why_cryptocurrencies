c2e0a6863214feb4cacb2fd7c92a3659bf83eedf
Jonas Hietala <mail@jonashietala.se>
2020-01-25 19:47:33 +0100

    Tokens

---
+++ tokens.html.pm
@@ -0,0 +1,20 @@
+#lang pollen
+
+◊(define-meta title "Tokens")
+◊(define-meta subtitle "How well do they work as money?")
+◊(define-meta published "2020-01-25T15:05:30+01:00")
+◊(define-meta updated "2020-01-25T15:05:37+01:00")
+◊(define-meta uuid "6eed25c0-a50e-4f50-86f4-bad049674817")
+
+Tokens
+
+◊ol{
+    ◊li{Giftcards}
+    ◊li{Bonus points}
+    ◊li{Bearer shares/bonds
+
+        ◊;https://www.investopedia.com/articles/bonds/08/bearer-bond.asp
+    }
+    ◊li{Stock options}
+}
+
