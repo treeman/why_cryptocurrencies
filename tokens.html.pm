d3aa74a2f9fe38531014eb4bceb3aa508f1eae68
Jonas Hietala <mail@jonashietala.se>
2020-01-26 19:33:57 +0100

    Words

--- tokens.html.pm
+++ tokens.html.pm
@@ -12,7 +12,7 @@ But would it really be completely useless? What if we had all the other good pro
 
 The most popular class of tokens are probably the ERC20 tokens, implemented on top of Ethereum. For example ◊link[usdc]{USD Coin}, Coinbase's stablecoin backed 1-to-1 by USD, is an ERC20 token. The ◊link[bnb]{Binance Coin}, used for the bonus system on a cryptocurrency exchange, is another example.◊sn{other-coins}
 
-Tokens can be used to raise funds, track bonus points, as gift cards or even to underpin derivative trading. In this chapter we'll look at some of them.
+Tokens can be used to raise funds, track bonus points, as gift cards or even to underpin derivative trading. I don't think they'll revolutionize any of these, but they may provide marginal improvements over the existing solutions. In this chapter we'll take a look at some of them.
 
 ◊ndef["other-coins"]{
     You could implement tokens on other cryptocurrencies as well. The ◊link[omni]{Omni Layer} is for example an extension network built on Bitcoin that supports tokens. The difference is that Ethereum's ERC20 tokens are miner validated, while Omni uses a different consensus mechanism.
@@ -49,7 +49,7 @@ Does it sound insane? That's really no different from how ◊link[kickstarter]{K
     You might still say it sounds insane, but just realize Kickstarter is very popular and it mostly works out well. (At least for board games, which I'm most familiar with.)
 }
 
-Why would you want a token for this? The benefit would be that anyone, anywhere, could participate and that participation could be done anonymously.◊sn{anon}
+Why would you want a token for this? The benefit would be that anyone, anywhere, could participate and that participation could be done anonymously. And it all takes place on a platform where you have a guarantee that you can sell your token if you lose faith in the project (since not even the issuer can prevent token transactions).◊sn{anon}
 
 ◊ndef["anon"]{
     It might be hard to claim anonymity if you want someone to send a board game to you, but it's relevant if you're expecting returns in cryptocurrencies or other digital goods.
@@ -64,7 +64,13 @@ Why would you want a token for this? The benefit would be that anyone, anywhere,
 
 ◊subhead{Better gift cards}
 
-What's the problem with gift cards?
+People use gift cards all the time, but they're not always that great. How do you verify that they're valid, and haven't already been used? How do you do that in stores, when you're choosing which one to buy, and how do you verify digital gift cards?
+
+For example let's say you walk into a store and want to buy a gift card. How do you know it's not fake, or haven't been used? Luckily there's a code on the gift card, which you can enter into a website to verify if it's still valid. Okay! It's valid, so you buy it and walk out the store.
+
+Unfortunately for you the store clerk has already written down the gift card code, and shortly after you leave the store he decides to use it up, making your gift card useless.
+
+◊;If you're lucky the company provides a way to verify them, maybe you can write in your code on a website to see if it's been used yet. But how do you know someone else has already copied your gift card, but just haven't used it yet? For example all
 
 ◊ul{
     ◊li{Verification}
@@ -82,4 +88,8 @@ Tokens allow you to create digital ◊link[bearer-share]{bearer shares}, where t
 
 ◊(define bearer-share "https://www.investopedia.com/terms/b/bearer_share.asp")
 ◊(define money-visualization "https://money.visualcapitalist.com/worlds-money-markets-one-visualization-2017/")
+
+
+◊subhead{But we can do this already?}
+
 
