d11d8dec47a5570d02e93973ffe39f66faacea21
Jonas Hietala <mail@jonashietala.se>
2020-01-26 07:35:24 +0100

    Words

--- tokens.html.pm
+++ tokens.html.pm
@@ -10,9 +10,9 @@ One of the most important properties that let cryptocurrencies function well as 
 
 But would it really be completely useless? What if we had all the other good properties, such as being able to instantly verify if they're valid, what would we have then? We would have ◊em{tokens}, and they aren't completely useless.
 
-We'll look at how they can be used to raise funds, as gift cards or even to underpin derivatives.
+The most popular class of tokens are probably the ERC20 tokens, implemented on top of Ethereum. For example ◊link[usdc]{USD Coin}, Coinbase's stablecoin backed 1-to-1 by USD, is an ERC20 token. The ◊link[bnb]{Binance Coin}, used for the bonus system on a cryptocurrency exchange, is another example.◊sn{other-coins}
 
-The most popular class of tokens are probably the ERC20 tokens, implemented on top of Ethereum. For example ◊link[usdc]{USD Coin}, Coinbase's stablecoin backed 1-to-1 by USD, is an ERC20 token. The ◊link[bnb]{Binance Coin} is another example.◊sn{other-coins}
+Tokens can be used to raise funds, track bonus points, as gift cards or even to underpin derivative trading. In this chapter we'll look at some of them.
 
 ◊ndef["other-coins"]{
     You could implement tokens on other cryptocurrencies as well. The ◊link[omni]{Omni Layer} is for example an extension network built on Bitcoin that supports tokens. The difference is that Ethereum's ERC20 tokens are miner validated, while Omni uses a different consensus mechanism.
@@ -20,6 +20,7 @@ The most popular class of tokens are probably the ERC20 tokens, implemented on t
 
 ◊;Tether plans to launch as an ERC20 token.
 
+◊(define brave "https://brave.com/")
 ◊(define usdc "https://www.coinbase.com/usdc")
 ◊(define bnb "https://coincentral.com/what-is-binance-coin-bnb/")
 ◊(define omni "https://www.omnilayer.org/")
@@ -30,13 +31,45 @@ The most popular class of tokens are probably the ERC20 tokens, implemented on t
 
 ◊subhead{Initial coin offering}
 
-The most famous, or infamous, use of tokens is for an ◊link[ico]{Initial Coin Offering (ICO)}. It's similar to an ◊link[ipo]{Initial Public Offering (IPO)}
+The most famous, or infamous, use of tokens is for an ◊link[ico]{Initial Coin Offering (ICO)}. It's similar to an ◊link[ipo]{Initial Public Offering (IPO)}, which aims to raise funds for companies or projects. In return for giving them money you'll receive a token as a sign of participation. ICOs are mostly unregulated and are easy for anyone to participate in.◊sn{security}
 
+◊ndef["security"]{
+    ICOs are classified as securities and the Securities and Exchange Commission (SEC) has intervened on a number of occasions.
+}
+
+One of the benefit of ICOs is that they're very easy to setup, and with the right tools it can be done in a few seconds. This is also a downside as there are countless of ICOs that function only as ◊link[pyramid-scheme]{pyramid schemes} (or other types of scams) with the only purpose of finding ◊link[greater-fools]{greater fools} to dump on.
+
+Although ICOs are mostly associated with scams (and rightfully so I'd say) there might be some usefulness here. What you're really doing is donating money to a cause you believe in, and hope you'll get a good outcome.
+
+◊;I'd say the ICO of the ◊link[brave]{privacy focused browser Brave} is an example of a good ICO.
+
+Does it sound insane? That's really no different from how ◊link[kickstarter]{Kickstarter} works, where it's very popular to pledge money for an interesting board game project and you wait for months or years and hope you'll get something in return.◊sn{insane?}
+
+◊ndef["insane?"]{
+    You might still say it sounds insane, but just realize Kickstarter is very popular and it mostly works out well. (At least for board games, which I'm most familiar with.)
+}
+
+Why would you want a token for this? The benefit would be that anyone, anywhere, could participate and that participation could be done anonymously.◊sn{anon}
+
+◊ndef["anon"]{
+    It might be hard to claim anonymity if you want someone to send a board game to you, but it's relevant if you're expecting returns in cryptocurrencies or other digital goods.
+}
+
+◊(define greater-fools "https://en.wikipedia.org/wiki/Greater_fool_theory")
+◊(define pyramid-scheme "https://en.m.wikipedia.org/wiki/Pyramid_scheme")
 ◊(define ico "https://www.investopedia.com/terms/i/initial-coin-offering-ico.asp")
 ◊(define ipo "https://www.investopedia.com/terms/i/ipo.asp")
+◊(define kickstarter "https://www.kickstarter.com/")
 
 
 ◊subhead{Better gift cards}
+
+What's the problem with gift cards?
+
+◊ul{
+    ◊li{Verification}
+    ◊li{Implementation}
+}
 
 
 ◊subhead{Derivates}
