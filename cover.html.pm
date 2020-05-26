9db7bd7a6a9a8d799ea969b389ac4db88036de58
Jonas Hietala <mail@jonashietala.se>
2020-05-26 22:13:27 +0200

    Cover page ideas

---
+++ cover.html.pm
@@ -0,0 +1,167 @@
+#lang pollen
+
+◊(define-meta title "Looking for a cover page")
+◊(define-meta no-side-space #t)
+◊(define-meta no-section-chapters-header #t)
+◊(define-meta skip-feed #t)
+
+I'm looking for a cover page for my book. There is an online version that's free for all to read, but I want to convert it to a physical book and an eBook (PDF and epub) and I'd like to commission a cover page for it.
+
+◊subhead{What I'm looking for}
+
+I'm looking for an original illustration, with a sort-of a cartoony feel that complements my own images inside the book (see below for examples).
+
+These pictures capture some of the feel I'm going for:
+
+◊figure[#:class "flex-33"]{
+    ◊raw-img[#:src "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.nostarch.com%2Fsites%2Fdefault%2Ffiles%2Fimagecache%2Fproduct_full%2Flyah.png&f=1&nofb=1" #:alt ""]{}
+    ◊raw-img[#:src "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fnostarch.com%2Fsites%2Fdefault%2Ffiles%2Fimagecache%2Fproduct_full%2Flisp.png&f=1&nofb=1" #:alt ""]{}
+    ◊raw-img[#:src "https://i.pinimg.com/236x/41/3c/12/413c1240c1d1ea431ee13982645157ab--art-store-art-pages.jpg" #:alt ""]{}
+}
+
+◊todo{List some covers with the feel I'm going for}
+
+I envision a cover you can look at and identify some of the topics I bring up in the book. Maybe a city or a small town where you can see these things:
+
+◊ul{
+    ◊li{Icons/symbols of cryptocurrencies.
+
+        Ones I've mentioned in the book are Bitcoin, Bitcoin Cash, Monero, Ethereum and Dogecoin:
+
+        ◊figure[#:class "flex-20"]{
+            ◊raw-img[#:src "/images/cover-help/btc.png" #:alt "Bitcoin"]{}
+            ◊raw-img[#:src "/images/cover-help/bch.png" #:alt "Bitcoin Cash"]{}
+            ◊raw-img[#:src "/images/cover-help/monero.png" #:alt "Monero"]{}
+            ◊raw-img[#:src "/images/cover-help/ethereum.png" #:alt "Ethereum"]{}
+            ◊raw-img[#:src "/images/cover-help/doge.png" #:alt "Dogecoin"]{}
+        }
+
+        Maybe hidden away as graffiti, or as a small sign hidden in a store window. The Bitcoin symbol should be easily identified somewhere on the cover, and preferably Monero as well.
+
+        They don't necessarily have to keep their colors, as long as they're identifiable. The Bitcoin unicode symbol "₿" could for example be used.
+    }
+    ◊li{A person with a ◊link[fawkes]{guy fawkes mask} (privacy)}
+    ◊li{Someone building a pyramid of bills (inflation)
+
+        ◊img[#:src "/images/german_notes.png" #:alt "bills"]{
+            An image I've used in the book. ◊link[stacks]{Inspired by this picture}.
+        }
+    }
+    ◊li{A banker running a printing press, spraying bills everywhere (financial crisis)}
+    ◊li{An S&M pair exiting a sex-toy shop (privacy)}
+    ◊li{Gambling, poker chips or blackjack? (provably fair gambling)}
+    ◊li{An homeless person holding a sign with a QR code (cashless dystopia)}
+    ◊li{A toilet door with a lock you have to use a credit card for, possibly next to the homeless person (cashless dystopia)}
+    ◊li{A police officer threatening someone to give them their stuff (protection against government confiscation)
+
+        ◊img[#:src "/images/gangster_cop.png" #:alt "Cop"]{
+            An image I've used for this scenario.
+        }
+    }
+    ◊li{People buying medicine in an alley (darknet markets)}
+    ◊li{A kid holding a bowl of soup (global currency)}
+    ◊li{Schrödinger's cat, with a marijuana symbol, stuck in a box (undesirable businesses)
+
+        The cat in the box is simultaneously dead and alive, you don't know until you open it.
+    }
+    ◊li{A blind man leading a pack of other blind people to their doom (blind leading the blind)}
+    ◊li{A rich pig (◊link[animal-farm]{like in Animal Farm})}
+    ◊li{◊link[delorean]{A DeLorean} (time machine in Back to The Future) (timestamping service)}
+    ◊li{A military helicopter firing at people (uncensorable donations, collateral murder)}
+    ◊li{Assange is stuck in the Ecuadorean embassy, with angry US officials outside. Maybe Trump, Obama and Bush? (uncensorable donations)}
+    ◊li{A farmer trading a pig for a chair (what is money?)
+
+        ◊img[#:src "/images/pig_chair_exchange.png" #:alt "Barter"]{
+            An image I've used in the book.
+        }
+    }
+    ◊li{Someone walking around with a bag of gold, or a gold bar (what is money?)}
+    ◊li{A snake oil salesman trying to sell poison (an skull on the bottle or something?) (look out for snake oil)}
+    ◊li{A lamborghini (common reference in the cryptocurrency sphere)}
+    ◊li{Other historical examples of money I've used, see ◊link[historical-examples-of-money]{this list of historical examples of money}.
+
+        For instance:
+
+        ◊ol{
+            ◊li{Slave trade using sea shells}
+            ◊li{A 3 meter big stone coin}
+            ◊li{A huge coin transported in a wheelbarrow}
+        }
+    }
+}
+
+The center of the cover could be a person who's lost, with a map in hand or question marks above the head, to signify that the book is about to explain these things.
+
+Clutter might become a problem here, so it's not necessary to include ◊em{all} these things.
+
+
+◊subhead{Images in the book}
+
+There are a few images in the book, and it would be awesome if the cover could match the cartoony style of them, but obviously massively improved.
+
+Here's a bunch of them:
+
+◊figure[#:class "flex-50"]{
+    ◊raw-img[#:src "/images/send_crypto.png" #:alt ""]{}
+    ◊raw-img[#:src "/images/sending_network.png" #:alt ""]{}
+}
+
+◊figure[#:class "flex-50"]{
+    ◊raw-img[#:src "/images/generals_decisions.png" #:alt ""]{}
+    ◊raw-img[#:src "/images/global.png" #:alt ""]{}
+}
+
+◊figure[#:class "flex-25"]{
+    ◊raw-img[#:src "/images/20kg_copper.png" #:alt ""]{}
+    ◊raw-img[#:src "/images/doge.png" #:alt ""]{}
+    ◊raw-img[#:src "/images/dancing.png" #:alt ""]{}
+    ◊raw-img[#:src "/images/bitcoin-priest.png" #:alt ""]{}
+}
+
+◊img[#:src "/images/pig_chair_exchange_long.png" #:alt ""]{}
+◊img[#:src "/images/xxx-payments2.png" #:alt ""]{}
+◊img[#:src "/images/counterfeiting2.png" #:alt ""]{}
+
+◊img[#:src "/images/unbanked-map.png" #:alt "" #:link #t]{}
+
+◊figure[#:class "flex-50"]{
+    ◊raw-img[#:src "/images/income-inequality-world.svg" #:alt ""]{}
+    ◊raw-img[#:src "/images/encrypted_mailbox.png" #:alt ""]{}
+}
+
+◊figure[#:class "flex-50"]{
+}
+
+◊figure[#:class "flex-50"]{
+    ◊raw-img[#:src "/images/coinjoin.png" #:alt ""]{}
+    ◊raw-img[#:src "/images/monero-transactions2.png" #:alt ""]{}
+}
+
+
+
+◊subhead{What I don't want}
+
+◊ul{
+    ◊li{Focus on a single cryptocurrency.
+
+        A big Bitcoin symbol as the cover, which is common for books on this topic, is for example off the table.
+    }
+}
+
+
+◊subhead{Other details}
+
+◊ol{
+    ◊li{I'm looking for someone experienced, to help me avoid common pitfalls.}
+    ◊li{I'll self-publish using ingram spark.}
+    ◊li{I'd like the raw image files.}
+    ◊li{The physical book should have a complete cover, for the front, back and side.}
+    ◊li{The cover should work for PDF and epub/mobi as well.}
+    ◊li{If you'd like I can pay you with a cryptocurrency of your choice.}
+}
+
+◊(define fawkes "https://duckduckgo.com/?t=ffab&q=guy+fawkes+mask&ia=images&iax=images")
+◊(define stacks "https://mashable.com/2016/07/27/german-hyperinflation/")
+◊(define delorean "https://duckduckgo.com/?q=delorean+back+to+the+future&t=ffab&iar=images&iax=images&ia=images")
+◊(define animal-farm "https://duckduckgo.com/?t=ffab&q=animal+farm+cover&iax=images&ia=images")
+
