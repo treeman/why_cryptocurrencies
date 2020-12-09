f626a0d49c96d4d23b20d4ad30e7c8c69cf0fdc9
Jonas Hietala <mail@jonashietala.se>
2020-12-09 08:42:36 +0100

    Update timestamps

--- extensions.html.pm
+++ extensions.html.pm
@@ -3,7 +3,7 @@
 ◊(define-meta title "Extensions")
 ◊(define-meta subtitle "Non-currency usage of a blockchain")
 ◊(define-meta published "2019-09-21T00:00:00+01:00")
-◊(define-meta updated "2020-10-24T11:38:30+02:00")
+◊(define-meta updated "2020-12-09T06:41:15+01:00")
 ◊(define-meta uuid "c83f3bb3-7277-43b0-ad8d-ee5ea4592a87")
 
 ◊(clear-sidenotes)
