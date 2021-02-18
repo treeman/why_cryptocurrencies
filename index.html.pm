9037c80a69e25900c37b8677b2794023413de5ac
Jonas Hietala <mail@jonashietala.se>
2021-02-18 22:23:11 +0100

    More landing page styling and design

--- index.html.pm
+++ index.html.pm
@@ -28,117 +28,150 @@
 }
 
 ◊section[#:class "what"]{
-  ELI5
+  ◊div[#:class "head"]{
+    ◊h1{What makes this book special?}
+  }
 
-  ◊ul{
-    ◊li{Have you ever wondered how Bitcoin works?}
-    ◊li{Or how}
+  ◊div[#:class "examples"]{
+    ◊ex["Gives you the important conceptual ideas" ""]{
+      It's all too easy to get bogged down with technical details; the high-level concepts are what's truly important. (Miners)
+    }
+
+    ◊ex["Clear and easy" ""]{
+      The book is beginner-friendly and doesn't require any prior knowledge. (?)
+    }
+
+    ◊ex["Focused on utility" ""]{
+      The focus is on concrete use-cases instead of the more seductive speculative approach. (Casino)
+    }
+
+    ◊ex["It's bigger than Bitcoin" ""]{
+      There are many more cryptocurrencies than Bitcoin. The focus is on the general cryptocurrency concept and it's potential instead of on a single implementation. (TRUST US)
+    }
+
+    ◊ex["Visits a wide variety of topics" ""]{
+      This isn't only about cryptocurrencies. It's also about money, censorship, our financial system and the dark secrets that Chelsea Manning and Edward Snowden revealed. (Snowden)
+    }
+
+    ◊ex["The order doesn't matter" ""]{
+      Chapters are self-contained so you can jump around and read the sections you're interested in. (Helicopters)
+    }
   }
 }
 
-◊book-type["print" "Print"]{
-  ◊ul[#:class "buttons"]{
-    ◊li{Buy from Amazon}
-    ◊li{Download Sample PDF}
+◊div[#:class "books"]{
+
+  ◊book-type["print" "Print"]{
+    ◊ul[#:class "buttons"]{
+      ◊li{Buy from Amazon}
+      ◊li{Download Sample PDF}
+    }
+
+    ◊ul[#:class "features"]{
+      ◊li{Beautiful design}
+      ◊li{Each page laid out by hand with utmost precision}
+      ◊li{Hand-drawn colored illustrations}
+      ◊li{ISBN: }
+    }
   }
 
-  ◊ul[#:class "features"]{
-    ◊li{Beautiful design}
-    ◊li{Each page laid out by hand with utmost precision}
-    ◊li{Hand-drawn colored illustrations}
-    ◊li{ISBN: }
+  ◊book-type["ebook" "eBook"]{
+    ◊ul[#:class "buttons"]{
+      ◊li{Buy directly from me with crypto}
+      ◊li{Kindle}
+      ◊li{iBooks}
+      ◊li{Play Books}
+      ◊li{Nook}
+      ◊li{EPUB}
+    }
+
+    ◊ul[#:class "features"]{
+      ◊li{Meticulously tuned CSS}
+      ◊li{ISBN: }
+    }
+  }
+  ◊book-type["pdf" "PDF"]{
+    ◊ul[#:class "buttons"]{
+      ◊li{Buy directly from me with crypto}
+      ◊li{Buy from X}
+      ◊li{Download Free Sample}
+    }
+
+    ◊ul[#:class "features"]{
+      ◊li{Same hand-crafted layout as the printed version}
+      ◊li{Looks the same on every reader and device}
+      ◊li{ISBN: }
+    }
+  }
+  ◊book-type["web" "Web"]{
+    ◊ul[#:class "buttons"]{
+      ◊li{Read Now}
+    }
+
+    ◊ul[#:class "features"]{
+      ◊li{Designed to look great on every device}
+      ◊li{Free!}
+      ◊li{Yes, the whole book is available to read online for free!}
+      ◊li{◊link[source-code]{The book is also open source}}
+    }
   }
 }
 
-◊book-type["ebook" "eBook"]{
-  ◊ul[#:class "buttons"]{
-    ◊li{Buy directly from me with crypto}
-    ◊li{Kindle}
-    ◊li{iBooks}
-    ◊li{Play Books}
-    ◊li{Nook}
-    ◊li{EPUB}
-  }
 
-  ◊ul[#:class "features"]{
-    ◊li{Meticulously tuned CSS}
-    ◊li{ISBN: }
-  }
-}
-◊book-type["pdf" "PDF"]{
-  ◊ul[#:class "buttons"]{
-    ◊li{Buy directly from me with crypto}
-    ◊li{Buy from X}
-    ◊li{Download Free Sample}
-  }
+◊div[#:class "user-comments"]{
+  ◊section[#:class "praise"]{
+    ◊qt[#:author "Roger Ver, the \"Bitcoin Jesus\""]{
+      Anyone who wishes to prepare for the future must learn about cryptocurrencies. This book is an excellent place to start.
+    }
 
-  ◊ul[#:class "features"]{
-    ◊li{Same hand-crafted design and layout as the printed version}
-    ◊li{Looks the same on every reader and device}
-    ◊li{ISBN: }
-  }
-}
-◊book-type["web" "Web"]{
-  ◊ul[#:class "buttons"]{
-    ◊li{Read Now}
-  }
+    ◊qt[#:author "SerHack, author of \"Mastering Monero\""]{
+      This book is a well-written resource that should be easy to understand by everyone. From the flaws of the current financial system to the future implications, you'll see what a cryptocurrency is and why it's needed.
+    }
 
-  ◊ul[#:class "features"]{
-    ◊li{Designed to look great on every device}
-    ◊li{Free!}
-    ◊li{Yes, the whole book is available to read online for free!}
-  }
-}
+    ◊qt[#:author "/u/MobTwo"]{
+      Education is the most powerful tool which you can use to change the world and investing your time reading this book may give you the highest dividends of your life.
+    }
 
 
-◊section[#:class "praise"]{
-  ◊qt[#:author "Roger Ver, the \"Bitcoin Jesus\""]{
-    Anyone who wishes to prepare for the future must learn about cryptocurrencies. This book is an excellent place to start.
-  }
+    ◊qt[#:author "/u/e-mess" #:url "https://www.reddit.com/r/Monero/comments/g60p91/my_book_why_cryptocurrencies_which_explains_the/fo82l84/"]{
+      Great book. I've been recommending it to everyone willing to learn about crypto.
+    }
 
-  ◊qt[#:author "SerHack, author of \"Mastering Monero\""]{
-    This book is a well-written resource that should be easy to understand by everyone. From the flaws of the current financial system to the future implications, you'll see what a cryptocurrency is and why it's needed.
-  }
+    ◊qt[#:author "Guarda Wallet" #:url "https://www.reddit.com/r/Monero/comments/g60p91/my_book_why_cryptocurrencies_which_explains_the/fo74gqm/"]{
+      The information is correct and concise. Going with what we saw, quality work has been done.
+    }
 
-  ◊qt[#:author "/u/MobTwo"]{
-    Education is the most powerful tool which you can use to change the world and investing your time reading this book may give you the highest dividends of your life.
-  }
+    ◊; ok
+    ◊qt[#:author "Josh McIntyre" #:url "https://www.reddit.com/r/CryptoCurrency/comments/g60pu0/my_book_why_cryptocurrencies_which_explains_the/fo7a7t0/"]{
+      Very cool! I love the focus on utility instead of just speculation. That is what truly gives cryptocurrencies their value.
+    }
 
-  ◊qt[#:author "/u/e-mess" #:url "https://www.reddit.com/r/Monero/comments/g60p91/my_book_why_cryptocurrencies_which_explains_the/fo82l84/"]{
-    Great book. I've been recommending it to everyone willing to learn about crypto.
-  }
+    ◊; ok
+    ◊qt[#:author "/u/n1ck9" #:url "https://www.reddit.com/r/btc/comments/g60oyw/my_book_why_cryptocurrencies_which_explains_the/fo7p0xq/"]{
+      This is very good, easy to read and nice illustrations!
+    }
 
-  ◊qt[#:author "Guarda Wallet" #:url "https://www.reddit.com/r/Monero/comments/g60p91/my_book_why_cryptocurrencies_which_explains_the/fo74gqm/"]{
-    The information is correct and concise. Going with what we saw, quality work has been done.
-  }
+    ◊; ok
+    ◊qt[#:author "/u/fatalglory" #:url "https://www.reddit.com/r/btc/comments/g60oyw/my_book_why_cryptocurrencies_which_explains_the/fo8yihw/"]{
+      Had a skim read of a few chapters. Looks like a great project, very impressive. You seem to have struck a great balance between giving enough information to be of practical help, while still keeping things simple enough that it's not overwhelming for people new to the space.
+    }
 
-  ◊qt[#:author "/u/pgh_ski" #:url "https://www.reddit.com/r/CryptoCurrency/comments/g60pu0/my_book_why_cryptocurrencies_which_explains_the/fo7a7t0/"]{
-    Very cool! I love the focus on utility instead of just speculation. That is what truly gives cryptocurrencies their value.
-  }
+    ◊; User suspended, so should be ok
+    ◊qt[#:author "/u/AntiqueRole" #:url "https://www.reddit.com/r/Monero/comments/dmsram/why_cryptocurrencies_the_financial_crisis_bad/f56bhmo/"]{
+      Well written based on facts. Really appreciate.
+    }
 
-  ◊; ok
-  ◊qt[#:author "/u/n1ck9" #:url "https://www.reddit.com/r/btc/comments/g60oyw/my_book_why_cryptocurrencies_which_explains_the/fo7p0xq/"]{
-    This is very good, easy to read and nice illustrations!
-  }
+    ◊; ok
+    ◊qt[#:author "/u/LovelyDay" #:url "https://www.reddit.com/r/btc/comments/dmsrju/why_cryptocurrencies_the_financial_crisis_bad/f54swd8/"]{
+      Great book, thanks for writing and making this available to all for free!
+    }
 
-  ◊qt[#:author "/u/fatalglory" #:url "https://www.reddit.com/r/btc/comments/g60oyw/my_book_why_cryptocurrencies_which_explains_the/fo8yihw/"]{
-    Had a skim read of a few chapters. Looks like a great project, very impressive. You seem to have struck a great balance between giving enough information to be of practical help, while still keeping things simple enough that it's not overwhelming for people new to the space.
-  }
+    ◊; ok
+    ◊qt[#:author "/u/Mercuun" #:url "https://www.reddit.com/r/CryptoCurrency/comments/bkavi8/how_do_cryptocurrencies_work/emfcxjr/"]{
+      I like how you explain the game theory behind it. Incentivising to play by the rules is -in my opinion- the most innovative aspect about blockchains. (as opposed to current financial markets where the incentive to cheat is quite big and only punishable by third actors but not the value of the fraudulent transactions themself)
+    }
 
-  ◊; User suspended, so should be ok
-  ◊qt[#:author "/u/AntiqueRole" #:url "https://www.reddit.com/r/Monero/comments/dmsram/why_cryptocurrencies_the_financial_crisis_bad/f56bhmo/"]{
-    Well written based on facts. Really appreciate.
   }
-
-  ◊qt[#:author "/u/LovelyDay" #:url "https://www.reddit.com/r/btc/comments/dmsrju/why_cryptocurrencies_the_financial_crisis_bad/f54swd8/"]{
-    Great book, thanks for writing and making this available to all for free!
-  }
-
-  ◊; ok
-  ◊qt[#:author "/u/Mercuun" #:url "https://www.reddit.com/r/CryptoCurrency/comments/bkavi8/how_do_cryptocurrencies_work/emfcxjr/"]{
-    I like how you explain the game theory behind it. Incentivising to play by the rules is -in my opinion- the most innovative aspect about blockchains. (as opposed to current financial markets where the incentive to cheat is quite big and only punishable by third actors but not the value of the fraudulent transactions themself)
-  }
-
 }
 
 ◊section[#:class "faq"]{
@@ -159,6 +192,13 @@
         (h1 ,type)
         ,@args)
 
-      (div ((class "right"))
-        (img ((src "/images/cover/front.png")))))
+      (div ((class "img-wrapper"))))
 )
+
+◊(define (ex title img . args)
+  `(div ((class "ex"))
+     (img ((src ,img)))
+
+     (div ((class "right"))
+       (h2 ,title)
+       ,@args)))
