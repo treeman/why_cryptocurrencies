84a16d9b2d735f116642d6e7d20b965d837d4f39
Jonas Hietala <mail@jonashietala.se>
2021-02-19 09:33:00 +0100

    Working towards a pretty landing page

--- index.html.pm
+++ index.html.pm
@@ -3,27 +3,29 @@
 ◊(define-meta template "landing.html")
 
 ◊section[#:class "hero"]{
-  ◊div[#:class "title"]{
-    ◊h1{◊|main-title|}
-    ◊h2{◊|subtitle|}
+  ◊div[#:class "header"]{
+    ◊div[#:class "title"]{
+      ◊h1{◊|main-title|}
+      ◊h2{◊|subtitle|}
 
-    ◊div[#:class "sell-it"]{
-      Bitcoin is one of the most transformative technologies in history, but at the same time it's dismissed as useless or only seen as a speculative vehicle. This book will dispel that myth by exploring real use-cases and show how cryptocurrencies are uniquely positioned to revolutionize the way we use money today.
+      ◊div[#:class "sell-it"]{
+        Bitcoin is one of the most transformative technologies in history, but at the same time it's dismissed as useless or only seen as a speculative vehicle. This book will dispel that myth by exploring real use-cases and show how cryptocurrencies are uniquely positioned to revolutionize the way we use money today.
 
-      ◊span[#:class "afford"]{Can you afford to miss out?}
+        ◊span[#:class "afford"]{Can you afford to miss out?}
+      }
+
+      ◊div[#:class "buy"]{
+        BUY THE BOOK
+      }
+
+      ◊div[#:class "buy"]{
+        Read it online for free
+      }
     }
 
-    ◊div[#:class "buy"]{
-      BUY THE BOOK
+    ◊div[#:class "book"]{
+      ◊raw-img[#:src "/images/cover/front.png" #:alt "Why Cryptocurrencies? Book"]{}
     }
-
-    ◊div[#:class "buy"]{
-      Read it online for free
-    }
-  }
-
-  ◊div[#:class "book"]{
-    ◊raw-img[#:src "/images/cover/front.png" #:alt "Why Cryptocurrencies? Book"]{}
   }
 }
 
@@ -33,28 +35,28 @@
   }
 
   ◊div[#:class "examples"]{
-    ◊ex["Gives you the important conceptual ideas" ""]{
-      It's all too easy to get bogged down with technical details; the high-level concepts are what's truly important. (Miners)
+    ◊ex["Gives you the important conceptual ideas" "concepts"]{
+      It's all too easy to get bogged down with technical details; the high-level concepts are what's truly important.
     }
 
-    ◊ex["Clear and easy" ""]{
-      The book is beginner-friendly and doesn't require any prior knowledge. (?)
+    ◊ex["Easy to read" "easy"]{
+      The book is beginner-friendly and doesn't require any prior knowledge.
     }
 
-    ◊ex["Focused on utility" ""]{
-      The focus is on concrete use-cases instead of the more seductive speculative approach. (Casino)
+    ◊ex["Focused on utility" "utility"]{
+      The focus is on concrete use-cases instead of the more seductive speculative approach.
     }
 
-    ◊ex["It's bigger than Bitcoin" ""]{
-      There are many more cryptocurrencies than Bitcoin. The focus is on the general cryptocurrency concept and it's potential instead of on a single implementation. (TRUST US)
+    ◊ex["It's bigger than Bitcoin" "general"]{
+      There are many more cryptocurrencies other than Bitcoin. The focus is on the general cryptocurrency concept and it's potential instead of on a single implementation. 
     }
 
-    ◊ex["Visits a wide variety of topics" ""]{
-      This isn't only about cryptocurrencies. It's also about money, censorship, our financial system and the dark secrets that Chelsea Manning and Edward Snowden revealed. (Snowden)
+    ◊ex["Visits a wide variety of topics" "variety"]{
+      This isn't only about cryptocurrencies. It's also about money, censorship, our financial system and the dark secrets that Chelsea Manning and Edward Snowden revealed.
     }
 
-    ◊ex["The order doesn't matter" ""]{
-      Chapters are self-contained so you can jump around and read the sections you're interested in. (Helicopters)
+    ◊ex["The order doesn't matter" "order"]{
+      Chapters are self-contained so you can jump around and read the sections you're interested in.
     }
   }
 }
@@ -119,21 +121,27 @@
 
 
 ◊div[#:class "user-comments"]{
+  ◊h1{Reader Comments}
+
   ◊section[#:class "praise"]{
+    ◊; ok
     ◊qt[#:author "Roger Ver, the \"Bitcoin Jesus\""]{
       Anyone who wishes to prepare for the future must learn about cryptocurrencies. This book is an excellent place to start.
     }
 
+    ◊; ok
     ◊qt[#:author "SerHack, author of \"Mastering Monero\""]{
       This book is a well-written resource that should be easy to understand by everyone. From the flaws of the current financial system to the future implications, you'll see what a cryptocurrency is and why it's needed.
     }
 
+    ◊; ok
     ◊qt[#:author "/u/MobTwo"]{
       Education is the most powerful tool which you can use to change the world and investing your time reading this book may give you the highest dividends of your life.
     }
 
-
-    ◊qt[#:author "/u/e-mess" #:url "https://www.reddit.com/r/Monero/comments/g60p91/my_book_why_cryptocurrencies_which_explains_the/fo82l84/"]{
+    ◊; ok
+    ◊qt[#:author "Michal Salaban"
+        #:src "creator of the Monero Python module"]{
       Great book. I've been recommending it to everyone willing to learn about crypto.
     }
 
@@ -176,15 +184,48 @@
 
 ◊section[#:class "faq"]{
   ◊h1{Frequently Asked Questions}
-}
 
-◊section[#:class "author"]{
-  ◊h1{Who Am I?}
+  ◊div[#:class "questions"]{
+    ◊faq["Do the different versions have different content?"]{
+      No! They all have the same chapters, the same illustrations and the same text. Even the free web version.
+    }
+
+    ◊faq["The book is too expensive, how can I get it cheaply?"]{
+      I made the web version completely free to make it available for as many people as possible. And if you buy the digital version with crypto you can pay whatever you want! That's almost free!
+    }
+
+    ◊faq["Which version pays you the most?"]{
+      Buying the digital versions directly from me with crypto removes all third-party fees, leaving me with the most amount of money in the end. It also allows you to set the amount to whatever you want, so if you want to shower me with money you can do that too!
+    }
+
+    ◊faq["Who made the amazing cover?"]{
+      Brad Lark, ◊(blark-email)
+    }
+
+    ◊faq["Can I get the cover as a poster?"]{
+      Unfortunately, not now. If there's demand for it I might look into it.
+    }
+
+    ◊faq["Can I get the cover as a background?"]{
+      Yes! ...
+    }
+
+    ◊faq["What if I find an error in the book?"]{
+      One of the benefits of an open-source book is that you can open an issue or send me a pull request ◊link[source-code]{on GitHub}. You can also contact me at ◊(link mailto email).
+    }
+  }
 }
 
 ◊section[#:class "ending"]{
-  ◊h1{Updated}
+  ◊(follow-section)
 }
+
+◊(define (follow-section)
+  `(section((class "follow"))
+     (div ((class "wip"))
+          "If you want to get updates on the book, consider subscribing:")
+     ,subscription-form
+     ))
 
 ◊(define (book-type c type . args)
    `(section ((class ,(string-append c " format")))
@@ -195,10 +236,22 @@
       (div ((class "img-wrapper"))))
 )
 
-◊(define (ex title img . args)
-  `(div ((class "ex"))
-     (img ((src ,img)))
+◊(define (ex title c . args)
+  `(div ((class ,(string-append c " ex")))
+     (div ((class "img-wrapper")))
 
      (div ((class "right"))
        (h2 ,title)
        ,@args)))
+
+◊(define (faq question . answer)
+   `(div ((class "question-answer"))
+      (div ((class "question"))
+         ,question)
+      (div ((class "answer"))
+         ,@answer)))
+
+◊(define (blark-email)
+  (define email "blark@blark.com")
+  `(a ((href ,(string-append "mailto:" email))) ,email))
+
